fd9064096910295366279e52bfb205fa8215a244
Gibran Hemani <explodecomputer@gmail.com>
2018-12-10 21:11:00 +0000

    implemented elastic check

--- app/resources/status.py
+++ app/resources/status.py
@@ -3,29 +3,52 @@ from flask_restful import Api, Resource
 from flask import request
 from _globals import *
 from _logger import *
+import requests
 
 class Status(Resource):
 	def get(self):
 		logger_info()
-		SQL   = "SELECT COUNT(*) FROM study_e;"
-		query = PySQLPool.getNewQuery(dbConnection)
-		query.Query(SQL)
-		logger.info("Testing logger")
-		logger2.debug("Testing logger2")
+		
 		out = {
 			'API version': VERSION, 
-			'Number of GWAS in MySQL': query.record[0]['COUNT(*)'],
-			'ElasticSearch status': 'Do something to test elasticsearch',
-			'LD reference panel': check_ld_ref(LD_REF),
-			'PLINK executable': check_plink(PLINK)
+			'Number of GWAS in MySQL': check_mysql(),
+			'ElasticSearch status': check_elastic(),
+			'LD reference panel': check_ld_ref(),
+			'PLINK executable': check_plink()
 		}
 		return out
 
-def check_ld_ref(ldref):
-	if(os.path.isfile(ldref+".bed") and os.path.isfile(ldref+".bim") and os.path.isfile(ldref+".fam")):
-		return ldref
+def check_ld_ref():
+	if(os.path.isfile(LD_REF+".bed") and os.path.isfile(LD_REF+".bim") and os.path.isfile(LD_REF+".fam")):
+		return "Available"
 	else:
-		return 'Reference unavailable'
+		return 'Unavailable'
 
-def check_plink(plink):
-	return os.popen(plink + " --version").read().split("\n")[0]
+def check_plink():
+	if os.popen(PLINK + " --version").read().split("\n")[0] == '':
+		return 'Unavailable'
+	else:
+		return "Available"
+
+def check_elastic():
+	try:
+		out = requests.get(es_conf['host'] + ':' + es_conf['port'] + '/_cluster/health?pretty').json()
+		if out['status'] == 'red':
+			return "Available"
+		else:
+			return "Unavailable"
+	except:
+		return "Unavailable"
+
+def check_mysql():
+	SQL   = "show databases;"
+	# SQL   = "SELECT COUNT(*) FROM study_e;"
+	try:
+		query = PySQLPool.getNewQuery(dbConnection)
+		query.Query(SQL)
+		if len(query.record) > 0:
+			return "Available"
+		else:
+			return "Unavailable"
+	except:
+		return "Unavailable"
