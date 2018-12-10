e133b56fd76b0e5d08ed035d31ea03b7b55b4d03
Gibran Hemani <explodecomputer@gmail.com>
2018-12-10 21:52:58 +0000

    Consolidated local and deploy credentials

--- app/resources/_globals.py
+++ app/resources/_globals.py
@@ -7,35 +7,24 @@ VERSION = '0.2.0'
 
 
 # Toggle for local vs deployed
-ES_CONF = "./conf_files/es_conf_local.json"
-# ES_CONF = "./conf_files/es_conf.json"
-
+APP_CONF = "./conf_files/app_conf.json"
 PLINK = './ld_files/plink1.90'
 LD_REF = './ld_files/data_maf0.01_rs'
 TMP_FOLDER = './tmp/'
 OAUTH2_URL = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='
 USERINFO_URL = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='
-MYSQL_DB = "./conf_files/mysql.json"
 LOG_FILE = "./logs/mrbaseapi.log"
 LOG_FILE_DEBUG = "./logs/mrbaseapi-debug.log"
 
-with open(MYSQL_DB) as f:
-	mrbase_config = json.load(f)
 
-dbConnection = PySQLPool.getNewConnection(**mrbase_config)
+with open(APP_CONF) as f:
+	app_config = json.load(f)
 
-with open(ES_CONF) as f:
-	es_conf = json.load(f)
-
-with open(MYSQL_DB) as f:
-	mrbase_config = json.load(f)
-
-dbConnection = PySQLPool.getNewConnection(**mrbase_config)
+dbConnection = PySQLPool.getNewConnection(**app_config['mysql'])
 
 #connect to elasticsearch
 es = Elasticsearch(
-		[{'host': es_conf['host'],'port': es_conf['port']}],
-		#http_auth=(es_conf['user'], es_conf['password']),
+		[{'host': app_config['es']['host'],'port': app_config['es']['port']}]
 )
 
 mrb_batch='MRB'
