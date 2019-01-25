94ced17e31e85fec291039b7f616cc33b4d91082
Matthew Lyon <ml@eduroam-70-97.nomadic.bris.ac.uk>
2019-01-25 12:20:54 +0000

    added gwasinfo ide

--- app/apis/gwasinfo.py
+++ app/apis/gwasinfo.py
@@ -14,6 +14,10 @@ from werkzeug.exceptions import BadRequest
 
 api = Namespace('gwasinfo', description="Get information about available GWAS summary datasets")
 
+params = GwasInfoNodeSchema.get_flask_model()
+params['comments'] = fields.String
+model = api.model('gwasinfo', params)
+
 parser1 = api.parser()
 parser1.add_argument(
     'X-Api-Token', location='headers', required=False, default='null',
@@ -87,10 +91,6 @@ parser3.add_argument(
     'X-Api-Token', location='headers', required=True,
     help='You must be authenticated to submit new GWAS data. To authenticate we use Google OAuth2.0 access tokens. The easiest way to obtain an access token is through the [TwoSampleMR R](https://mrcieu.github.io/TwoSampleMR/#authentication) package using the `get_mrbase_access_token()` function.')
 
-params = GwasInfoNodeSchema.get_flask_model()
-params['comments'] = fields.String
-model = api.model('gwasinfo', params)
-
 
 @api.route('/add')
 @api.expect(parser3)
@@ -105,11 +105,13 @@ class GwasInfoAdd(Resource):
         logger_info()
 
         try:
+            # TODO ? race condition
             req = request.get_json()
+            req['id'] = GwasInfo.get_next_numeric_id()
 
             # load
             user = User(self.user_schema.load({"uid": get_user_email(request.headers.get('X-Api-Token'))}))
-            study = GwasInfo(self.study_schema.load(req))
+            gwasinfo = GwasInfo(self.study_schema.load(req))
 
             try:
                 props = self.added_rel.load({'epoch': time.time(), "comments": req['comments']})
@@ -118,20 +120,19 @@ class GwasInfoAdd(Resource):
 
             # persist or update
             user.create_node()
-            study.create_node()
+            gwasinfo.create_node()
 
             # link study to user; record epoch and optional comments
             rel = AddedByRel(**props)
-            rel.create_rel(study, user)
+            rel.create_rel(gwasinfo, user)
 
-            # TODO return gwasinfo identifier
-
+            return {"id": gwasinfo['id']}, 200
 
         except marshmallow.exceptions.ValidationError as e:
             raise BadRequest("Could not validate payload: {}".format(e))
 
 
-parser3.add_argument('gwas_info_identifier', location='files', type=FileStorage, required=True,
+parser3.add_argument('gwas_info_identifier', type=str, required=True,
                      help="Identifier to which the summary stats belong.")
 parser3.add_argument('file', location='files', type=FileStorage, required=True,
                      help="Path to GWAS summary stats text file for upload.")
@@ -152,7 +153,7 @@ parser3.add_argument('cases_idx', type=int, required=False,
                      help="Column index for number of cases (if case-control study); 0-indexed)")
 
 
-@api.route('/', methods=["post"])
+@api.route('/upload', methods=["post"])
 @api.doc(description="Upload GWAS summary stats file")
 class UploadGwasSummaryStatsResource(Resource):
 
@@ -163,7 +164,9 @@ class UploadGwasSummaryStatsResource(Resource):
 
         # read file
         contents = uploaded_file.read()
+        print(contents)
 
         # TODO validate file
+        # TODO save file in triage
 
         return {'message': 'Upload successful'}, 201
