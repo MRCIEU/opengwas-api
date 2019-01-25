df47a487eb8b1e294b31935b6525b8b181480b82
Matthew Lyon <ml@eduroam-70-97.nomadic.bris.ac.uk>
2019-01-25 11:23:14 +0000

    switched study to gwasinfo

--- app/apis/gwasinfo.py
+++ app/apis/gwasinfo.py
@@ -1,6 +1,15 @@
-from flask_restplus import Resource, reqparse, Namespace
+from flask_restplus import Resource, reqparse, Namespace, fields
 from resources._logger import *
 from queries.cql_queries import *
+from schemas.gwas_info_node_schema import GwasInfoNodeSchema
+from schemas.user_node_schema import UserNodeSchema
+from schemas.added_by_rel_schema import AddedByRelSchema
+from queries.user_node import User
+from queries.gwas_info_node import GwasInfo
+from queries.added_by_rel import AddedByRel
+import time
+import marshmallow.exceptions
+from werkzeug.exceptions import BadRequest
 
 api = Namespace('gwasinfo', description="Get information about available GWAS summary datasets")
 
@@ -70,3 +79,49 @@ class GwasInfoPost(Resource):
                 except LookupError as e:
                     continue
             return recs
+
+
+parser3 = api.parser()
+parser3.add_argument(
+    'X-Api-Token', location='headers', required=True,
+    help='You must be authenticated to submit new GWAS data. To authenticate we use Google OAuth2.0 access tokens. The easiest way to obtain an access token is through the [TwoSampleMR R](https://mrcieu.github.io/TwoSampleMR/#authentication) package using the `get_mrbase_access_token()` function.')
+
+params = GwasInfoNodeSchema.get_flask_model()
+params['comments'] = fields.String
+model = api.model('study', params)
+
+
+@api.route('/add')
+@api.expect(parser3)
+@api.doc(description="Add new gwas information")
+class StudyResource(Resource):
+    study_schema = GwasInfoNodeSchema()
+    user_schema = UserNodeSchema()
+    added_rel = AddedByRelSchema()
+
+    @api.expect(model, validate=False)
+    def post(self):
+        logger_info()
+
+        try:
+            req = request.get_json()
+
+            # load
+            user = User(self.user_schema.load({"uid": get_user_email(request.headers.get('X-Api-Token'))}))
+            study = GwasInfo(self.study_schema.load(req))
+
+            try:
+                props = self.added_rel.load({'epoch': time.time(), "comments": req['comments']})
+            except KeyError:
+                props = self.added_rel.load({'epoch': time.time()})
+
+            # persist or update
+            user.create_node()
+            study.create_node()
+
+            # link study to user; record epoch and optional comments
+            rel = AddedByRel(**props)
+            rel.create_rel(study, user)
+
+        except marshmallow.exceptions.ValidationError as e:
+            raise BadRequest("Could not validate payload: {}".format(e))
--- app/queries/study_node.py
+++
@@ -1,7 +0,0 @@
-from queries.unique_node import UniqueNode
-from schemas.study_node_schema import StudyNodeSchema
-
-
-class Study(UniqueNode):
-    _UID_KEY = 'id'
-    _SCHEMA = StudyNodeSchema
