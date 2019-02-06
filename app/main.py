85b03584015a7b5966c05749e4b0a6c3eb899921
Matthew Lyon <ml@eduroam-70-97.nomadic.bris.ac.uk>
2019-02-06 13:00:01 +0000

    added report view

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from apis import api
 from resources._globals import app_config
 from resources._neo4j import Neo4j
 
+app = Flask(__name__, static_folder="reports")
 
-def main():
-    app = Flask(__name__)
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
+app.teardown_appcontext(Neo4j.close_db)
+api.init_app(app)
 
-    app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
-    app.teardown_appcontext(Neo4j.close_db)
-    api.init_app(app)
 
-    app.run(host='0.0.0.0', debug=True, port=app_config['flask']['port'])
+@app.route('/reports/<path:path>')
+def send_js(path):
+    return send_from_directory('reports', path)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', debug=True, port=app_config['flask']['port'])
