5ed44bf2c801af12e3256123824bda144152f66d
Matthew Lyon <ml@eduroam-70-97.nomadic.bris.ac.uk>
2019-02-14 10:04:06 +0000

    added bgc index

--- app/queries/gwas_info_node.py
+++ app/queries/gwas_info_node.py
@@ -1,7 +1,17 @@
 from queries.unique_node import UniqueNode
 from schemas.gwas_info_node_schema import GwasInfoNodeSchema
+from resources._neo4j import Neo4j
 
 
 class GwasInfo(UniqueNode):
     _UID_KEY = 'id'
     _SCHEMA = GwasInfoNodeSchema
+
+    # TODO returns None
+    @classmethod
+    def get_next_numeric_id(cls):
+        tx = Neo4j.get_db()
+        results = tx.run(
+            "MATCH (n:" + cls.get_node_label() + ") WHERE n." + cls._UID_KEY + " =~ 'bgc-*' RETURN max(toInteger(substring(n." + cls._UID_KEY + ", 4))) + 1 as uid;"
+        )
+        return results.single()['uid']
