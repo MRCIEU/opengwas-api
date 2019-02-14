de7ce199b081371febe20bafea9fa7c0e9826b18
Matthew Lyon <ml@eduroam-70-97.nomadic.bris.ac.uk>
2019-02-14 10:04:06 +0000

    added bgc index

--- app/queries/tests/test_cql_queries.py
+++ app/queries/tests/test_cql_queries.py
@@ -62,7 +62,10 @@ def test_add_new_gwas_info():
     with app.app_context():
         # check returns id
         uid = add_new_gwas('g.hemani@bristol.ac.uk', d)
+        print(uid)
         assert int(uid) > 0
+
+        # TODO now returns string
 
         # TODO check new gwas requires qc
 
