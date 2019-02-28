7c7695f6bd6c26d915da2dce5559e08d35153c5f
Gibran Hemani <explodecomputer@gmail.com>
2019-02-28 12:30:17 +0000

    fixes #35 and fixes #36

--- app/apis/tests/test_ld.py
+++ app/apis/tests/test_ld.py
@@ -5,11 +5,49 @@ def test_clumping1(url):
     payload = {'rsid': ['rs756190', 'rs7526762', 'rs7601028', 'rs7622475', 'rs9300092', 'rs9309995'],
                'pval': [1.607e-33, 4.813e-15, 1.502e-29, 3.961e-14, 2.246e-08, 9.035e-10]}
     r = requests.post(url + "/ld/clump", data=payload)
-    assert r.status_code == 200 and len(r.json()) < 6 and len(r.json()) > 0
-
+    assert r.status_code == 200
+    assert len(r.json()) < 6
+    assert len(r.json()) > 0
 
 def test_clumping2(url):
     payload = {'rsid': ['rs756190', 'rs7526762', 'rs7601028', 'rs7622475', 'rs9300092', 'rs9309995'],
                'pval': [1.607e-33, 4.813e-15, 1.502e-29, 3.961e-14, 2.246e-08, 9.035e-10], 'pthresh': 1e-10}
     r = requests.post(url + "/ld/clump", data=payload)
-    assert r.status_code == 200 and len(r.json()) <= 4 and len(r.json()) > 0
+    assert r.status_code == 200
+    assert len(r.json()) <= 4
+    assert len(r.json()) > 0
+
+def test_clumping3(url):
+    payload = {'rsid': ['rs4988235', 'rs182549'],
+               'pval': [5e-10, 5e-9]}
+    r = requests.post(url + "/ld/clump", data=payload)
+    assert r.status_code == 200
+    assert len(r.json()) == 1
+
+def test_matrix1(url):
+	payload = {'rsid': ['rs756190', 'rs7526762', 'rs7601028', 'rs7622475', 'rs9300092', 'rs9309995']}
+	r = requests.post(url + "/ld/matrix", data=payload)
+	o = r.json()
+	assert r.status_code == 200
+	assert 'snplist' in o
+	assert 'matrix' in o
+	assert len(o['snplist']) == len(o['matrix'])
+
+def test_matrix2(url):
+	payload = {'rsid': ['rs4988235', 'rs182549']}
+	r = requests.post(url + "/ld/matrix", data=payload)
+	o = r.json()
+	assert r.status_code == 200
+	assert 'snplist' in o
+	assert 'matrix' in o
+	assert len(o['snplist']) == len(o['matrix'])
+
+def test_matrix3(url):
+	payload = {'rsid': ['FAKESNP', 'FAKESNP2']}
+	r = requests.post(url + "/ld/matrix", data=payload)
+	o = r.json()
+	assert r.status_code == 200
+	assert 'snplist' in o
+	assert 'matrix' in o
+	assert len(o['snplist']) == len(o['matrix'])
+
