a59fc27c4ec25332ae42c68a8a26f82dccaa7070
Matthew Lyon <ml@eduroam-202-84.nomadic.bris.ac.uk>
2019-03-01 09:06:01 +0000

    began adding redis

--- app/data_processing/harmonise.py
+++ app/data_processing/harmonise.py
@@ -1,6 +1,22 @@
-class Harmonise:
-    def harmonise_against_genome(self):
+import subprocess
+import os
+
+
+class Harmonisation:
+    def launch_reference_panel_harmonisation(self):
+        # TODO
         raise NotImplemented
 
-    def harmonise_against_reference_panel(self):
-        raise NotImplemented
+    @staticmethod
+    def launch_genome_harmonisation(id, gwas_file, chr_col, pos_col, snp_col, ea_col, oa_col, eaf_col, beta_col, se_col,
+                                    pval_col, ncontrol_col, ncase_col, delimiter, header, gzipped):
+
+        if
+
+        process = [
+            "docker",
+            "run",
+            "-o", None,
+            "-g", gwas_file,
+        ]
+        subprocess.run(process, capture_output=True)
