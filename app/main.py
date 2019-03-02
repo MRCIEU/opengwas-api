f9bc6b712e01cc72a3668db1dc78cd668d7d47bd
Matt Lyon <ml18692@bristol.ac.uk>
2019-03-02 15:46:19 +0000

    removed middlewre

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ from flask import Flask, send_from_directory
 from apis import api
 from resources._globals import app_config
 from resources._neo4j import Neo4j
-from reverse_proxied import ReverseProxied
 
 app = Flask(__name__, static_folder="reports")
-app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 app.teardown_appcontext(Neo4j.close_db)
