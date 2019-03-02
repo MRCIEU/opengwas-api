3bc3fe76de4081ae1b78d3e866e90a2d900ec20d
Gibran Hemani <explodecomputer@gmail.com>
2019-03-02 23:51:52 +0000

    added about page

--- app/apis/status.py
+++ app/apis/status.py
@@ -1,3 +1,4 @@
+from flask import g
 from flask_restplus import Namespace, Resource
 from resources._globals import *
 from resources._logger import *
@@ -11,16 +12,20 @@ api = Namespace('status', description="Status of API and linked resources")
 @api.doc(description="Check services are running")
 class Status(Resource):
     def get(self):
-        logger_info()
-        out = {
-            'API version': VERSION,
-            'Neo4j status': Neo4j.check_running(),
-            'ElasticSearch status': check_elastic(),
-            'LD reference panel': check_ld_ref(),
-            'PLINK executable': check_plink()
-        }
-        return out
 
+        logger_info()
+        return check_all()
+
+
+def check_all():
+    out = {
+        'API version': VERSION,
+        'Neo4j status': Neo4j.check_running(),
+        'ElasticSearch status': check_elastic(),
+        'LD reference panel': check_ld_ref(),
+        'PLINK executable': check_plink()
+    }
+    return out
 
 def check_ld_ref():
     if (os.path.isfile(LD_REF + ".bed") and os.path.isfile(LD_REF + ".bim") and os.path.isfile(LD_REF + ".fam")):
@@ -36,8 +41,24 @@ def check_plink():
         return "Available"
 
 
+def count_elastic_records():
+    url = 'http://' + app_config['es']['host'] + ':' + str(app_config['es']['port']) + '/_stats/docs'
+    try:
+        out = requests.get(url).json()['_all']['primaries']['docs']['count']
+        print(out)
+        return out
+    except Exception as e:
+        return "Error"
+
+def count_neo4j_datasets():
+    tx = Neo4j.get_db()
+    res = tx.run("MATCH (n:GwasInfo) return count(n) as n").single()[0]
+    return(res)
+
+
 def check_elastic():
     url = 'http://' + app_config['es']['host'] + ':' + str(app_config['es']['port']) + '/_cluster/health?pretty'
+    count_elastic_records()
     try:
         out = requests.get(url).json()
         if out['status'] == 'red':
@@ -46,3 +67,5 @@ def check_elastic():
             return "Available"
     except Exception as e:
         return "Error"
+
+
