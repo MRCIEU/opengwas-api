a9e7442be338e44f7215515a60aa2644965b7aa3
Matthew Lyon <ml@vpn-user-244-101.nomadic.bris.ac.uk>
2019-03-05 09:35:14 +0000

    added var for hiding private endpoints

--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -7,17 +7,22 @@ from .phewas import api as phewas
 from .tophits import api as tophits
 from .quality_control import api as quality_control
 from .edit import api as edit
-from resources._globals import VERSION
+from resources._globals import VERSION, APP_CONF
 from .ld import api as ld
 
 api = Api(version=VERSION, title='Bristol GWAS Collection',
-          description='A RESTful API for querying thousands of GWAS summary datasets', docExpansion='full', doc='/docs/')
+          description='A RESTful API for querying thousands of GWAS summary datasets', docExpansion='full',
+          doc='/docs/')
 
+# public
 api.add_namespace(status)
 api.add_namespace(assoc)
 api.add_namespace(phewas)
 api.add_namespace(tophits)
 api.add_namespace(ld)
 api.add_namespace(gwasinfo)
-api.add_namespace(quality_control)
-api.add_namespace(edit)
+
+# private
+if APP_CONF['enable_private_endpoints']:
+    api.add_namespace(quality_control)
+    api.add_namespace(edit)
