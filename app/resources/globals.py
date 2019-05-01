fda10fc70e51dd026aea6f2836d8143ba1ecfe22
Matthew Lyon <ml@vpn-user-247-039.nomadic.bris.ac.uk>
2019-05-01 15:24:27 +0100

    added print

--- app/resources/globals.py
+++ app/resources/globals.py
@@ -3,6 +3,7 @@ import json
 import platform
 import os
 from neo4j import GraphDatabase
+import logging
 
 
 class Globals:
@@ -29,10 +30,13 @@ class Globals:
         try:
             if os.environ['ENV'] == 'production':
                 app_config = app_config['production']
+                print("Using production settings")
             else:
                 app_config = app_config['local']
+                print("Using local settings")
         except KeyError as e:
             app_config = app_config['local']
+            print("Using local settings")
 
         try:
             if os.environ['ACCESS'] == 'public':
