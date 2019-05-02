a74bd9922cc2eeed891b1013e2fc27d324129c01
Matthew Lyon <ml@vpn-user-247-039.nomadic.bris.ac.uk>
2019-05-02 09:51:31 +0100

    logging

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ import logging
 
 app = flask.Flask(__name__, static_folder="static")
 
+
 def main():
     setup_logger('event-log', Globals.LOG_FILE)
     logger = setup_logger('debug-log', Globals.LOG_FILE_DEBUG, level=logging.DEBUG)
@@ -20,7 +21,7 @@ def main():
     app.teardown_appcontext(Neo4j.close_db)
     api.init_app(app)
 
-    logger.info("Starting MRB API")
+    logger.info("Starting MRB API v{}".format(Globals.VERSION))
     app.run(host='0.0.0.0', port=Globals.app_config['flask']['port'])
 
 
