3e3d7cc5069e5bc3982dac81390a7e3914e694fd
Matthew Lyon <ml@vpn-user-247-039.nomadic.bris.ac.uk>
2019-05-02 13:22:04 +0100

    updated deploy

--- production.sh
+++ production.sh
@@ -2,45 +2,44 @@
 set -euo pipefail
 
 # ensure we're up to date
-# git pull
+git pull
 
 # stop and rm
 docker stop mrb-neo4j || true
 docker rm mrb-neo4j || true
 docker stop mr-base-api-restpluspy3 || true
 docker rm mr-base-api-restpluspy3 || true
+docker network rm  mrb-net || true
 
 # get LD data
-#wget -O app/ld_files.tgz https://www.dropbox.com/s/yuo7htp80hizigy/ld_files.tgz?dl=0
-#tar xzvf app/ld_files.tgz -C app/
-#rm app/ld_files.tgz
+wget -O app/ld_files.tgz https://www.dropbox.com/s/yuo7htp80hizigy/ld_files.tgz?dl=0
+tar xzvf app/ld_files.tgz -C app/
+rm app/ld_files.tgz
 
 # build latest api image
-#docker build -t mr-base-api-restpluspy3:latest .
+docker build -t mr-base-api-restpluspy3:latest .
 
-# deploy database
+# create database container
 docker create \
 --name mrb-neo4j \
 --restart always \
 -p 27474:7474 \
 -p 27687:7687 \
 -p 27473:7473 \
--v /data/bgc:/data/bgc \
--v /data/mrb_logs:/data/mrb_logs \
+-v /data/mrb_neo4j/data:/data \
+-v /data/mrb_neo4j/logs:/logs \
 -e NEO4J_AUTH=neo4j/dT9ymYwBsrzd \
 -e NEO4J_dbms_memory_heap_max__size=10G \
 -e NEO4J_dbms_memory_heap_initial__size=5G \
 -e NEO4J_dbms_memory_pagecache_size=10G \
 -e NEO4J_dbms_shell_enabled=true \
 -e NEO4J_dbms_allow__upgrade=true \
--v /data/mrb_neo4j/data:/data \
--v /data/mrb_neo4j/logs:/logs \
 neo4j:3.5
 
-# deploy flask
+# create api container
 docker create \
---restart always \
 --name mr-base-api-restpluspy3 \
+--restart always \
 -p 8082:80 \
 -v /data/bgc:/data/bgc \
 -v /data/mrb_logs:/data/mrb_logs \
