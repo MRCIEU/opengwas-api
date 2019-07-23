d111120456a43df0b5e97bcc46f2019f9a96c165
Matt Lyon <ml18692@bristol.ac.uk>
2019-07-23 16:14:29 +0100

    added prject name

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# project name
+COMPOSE_PROJECT_NAME="mr-base-api-test"
+
 # get TwoSampleMR token
-## populate first ##
 token=$(cat ./token.temp)
 
 # get test data for graph
-mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p -B -N -e "select * from study_e" mrbase > /tmp/study_e.tsv
-mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p -B -N -e "select * from groups" mrbase > /tmp/groups.tsv
-mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p -B -N -e "select * from permissions_e" mrbase > /tmp/permissions_e.tsv
-mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p -B -N -e "select * from memberships" mrbase > /tmp/memberships.tsv
+mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p'M1st3rbase!' -B -N -e "select * from study_e" mrbase > /tmp/study_e.tsv
+mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p'M1st3rbase!' -B -N -e "select * from groups" mrbase > /tmp/groups.tsv
+mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p'M1st3rbase!' -B -N -e "select * from permissions_e" mrbase > /tmp/permissions_e.tsv
+mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p'M1st3rbase!' -B -N -e "select * from memberships" mrbase > /tmp/memberships.tsv
 
 # build test stack
 docker-compose -f ./docker-compose-test.yml up -d
