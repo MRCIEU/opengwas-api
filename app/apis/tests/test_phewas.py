d1e44b681fc086142d8461ce86f70f54fa1d3e52
Matthew Lyon <matt.lyon@bristol.ac.uk>
2019-08-02 15:02:39 +0100

    Update test_phewas.py

--- app/apis/tests/test_phewas.py
+++ app/apis/tests/test_phewas.py
@@ -7,7 +7,7 @@ token = get_mrbase_access_token()
 ## GET
 
 def test_phewas_get1(url):
-    r = requests.get(url + "/phewas/rs234/0.0001")
+    r = requests.get(url + "/phewas/rs234/0.001")
     assert r.status_code == 200 and len(r.json()['data']) > 15
 
 def test_phewas_get2(url):
@@ -19,7 +19,7 @@ def test_phewas_get2(url):
 
 def test_phewas_post1(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'rsid': 'rs234', 'pval': 0.0001}
+    payload = {'rsid': 'rs234', 'pval': 0.001}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()['data']) > 15
 
