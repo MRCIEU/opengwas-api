3d3141df7342890b4ff4656dd31caa7c849d9451
Gibran Hemani <explodecomputer@gmail.com>
2019-09-03 15:56:07 +0100

    added tests for variants endpoint

---
+++ app/apis/tests/test_variants.py
@@ -0,0 +1,53 @@
+import requests
+
+## GET
+
+def test_rsid_get1(url):
+    r = requests.get(url + "/variants/rsid/rs234")
+    assert r.status_code == 200 and len(r.json()['results']) == 1
+
+def test_rsid_get2(url):
+    r = requests.get(url + "/variants/rsid/rs234,rs333")
+    assert r.status_code == 200 and len(r.json()['results']) == 2
+
+def test_chrpos_get1(url):
+    r = requests.get(url + "/variants/chrpos/7:105561135")
+    assert r.status_code == 200 and len(r.json()['results']) == 1
+
+def test_chrpos_get2(url):
+    r = requests.get(url + "/variants/chrpos/7:105561135?radius=1000")
+    assert r.status_code == 200 and r.json()[0]['7:105561135']['total'] > 1
+
+def test_gene_get1(url):
+    r = requests.get(url + "/variants/gene/ENSG00000123374")
+    assert r.status_code == 200 and len(r.json()['results']) > 1
+
+
+# ## POST
+
+
+def test_rsid_post1(url):
+    payload = {'rsid': ['rs234', 'rs333']}
+    r = requests.post(url + "/variants/rsid", data=payload)
+    print(r.json())
+    assert r.status_code == 200 and len(r.json()['results']) == 2
+
+
+def test_chrpos_post1(url):
+    payload = {'chrpos': ['7:105561135', '10:44865737']}
+    r = requests.post(url + "/variants/chrpos", data=payload)
+    print(r.json())
+    assert r.status_code == 200 and len(r.json()['results']) == 2
+
+
+def test_chrpos_post2(url):
+    payload = {'chrpos': ['7:105561135', '10:44865737']}
+    r = requests.post(url + "/variants/chrpos", data=payload)
+    o = r.json()
+    assert r.status_code == 200 and len(r.json()['results']) == 2
+
+def test_chrpos_post2(url):
+    payload = {'chrpos': ['7:105561135', '10:44865737'], 'radius':100}
+    r = requests.post(url + "/variants/chrpos", data=payload)
+    o = r.json()
+    assert r.status_code == 200 and len(r.json()) == 2
--- app/apis/variants.py
+++ app/apis/variants.py
@@ -27,7 +27,6 @@ class VariantGet(Resource):
 
 parser2 = reqparse.RequestParser()
 parser2.add_argument('rsid', required=False, type=str, action='append', default=[], help="List of variant rs IDs")
-parser2.add_argument('chrpos', required=False, type=str, action='append', default=[], help="List of variant chr:pos format on build 37 (e.g. 7:105561135)")
 
 @api.route('/rsid')
 @api.doc(
@@ -63,25 +62,58 @@ class VariantPost(Resource):
 parser1 = reqparse.RequestParser()
 parser1.add_argument('radius', type=int, required=False, default=0, help="Range to search either side of target locus")
 
-@api.route('/chrpos/<int:chr>/<int:pos>')
+@api.route('/chrpos/<chrpos>')
 @api.expect(parser1)
 @api.doc(
-    description="Obtain information for a particular SNP or comma separated list of SNPs",
+    description="Obtain information for a particular variant or comma separated list of variants",
     params={
-        'chr': 'B37 chromosome',
-        'pos': 'B37 position'
+        'chrpos': 'Comma separated B37 coordinates e.g. 7:105561135,10:44865737'
     }
 )
-class RangeGet(Resource):
-    def get(self, chr=None, pos=None, radius=0):
+class ChrposGet(Resource):
+    def get(self, chrpos, radius=0):
         args = parser1.parse_args()
+        chrpos = [x for x in ''.join(chrpos.split()).split(',')]
         try:
-            total, hits = range_query(chr, pos, args['radius'])
+            out = range_query(chrpos, args['radius'])
         except Exception as e:
             logger.error("Could not obtain variant information: {}".format(e))
             abort(503)
-        return {"total":total,"results":hits}
+        return out
 
+
+parser3 = reqparse.RequestParser()
+parser3.add_argument('chrpos', required=False, type=str, action='append', default=[], help="List of variant chr:pos format on build 37 (e.g. 7:105561135)")
+parser3.add_argument('radius', type=int, required=False, default=0, help="Range to search either side of target locus")
+
+@api.route('/chrpos')
+@api.doc(
+    description="""
+Obtain information for a particular variant or comma separated list of variants. Note the payload can be passed to curl via json using e.g.:
+
+```
+-X POST -d '
+{
+    'chrpos': ['7:105561135','10:44865737']
+}
+'
+```
+
+"""
+)
+class ChrposPost(Resource):
+    @api.expect(parser3)
+    def post(self):
+        args = parser3.parse_args()
+
+        if (len(args['chrpos']) == 0):
+            abort(405)
+        try:
+            out = range_query(args['chrpos'], args['radius'])
+        except Exception as e:
+            logger.error("Could not obtain variant information: {}".format(e))
+            abort(503)
+        return out
 
 
 
@@ -104,53 +136,3 @@ class GeneGet(Resource):
         return {"total":total,"results":hits}
 
 
-
-
-# parser2 = reqparse.RequestParser()
-# parser2.add_argument('rsid', required=False, type=str, action='append', default=[], help="List of SNP rs IDs")
-# parser2.add_argument('id', required=False, type=str, action='append', default=[], help="list of MR-Base GWAS study IDs")
-# parser2.add_argument('proxies', type=int, required=False, default=0, help="Whether to look for proxies (1) or not (0)")
-# parser2.add_argument('r2', type=float, required=False, default=0.8, help="Minimum LD r2 for a proxy")
-# parser2.add_argument('align_alleles', type=int, required=False, default=1, help="Whether to align alleles")
-# parser2.add_argument('palindromes', type=int, required=False, default=1, help="Whether to allow palindromic proxies")
-# parser2.add_argument('maf_threshold', type=float, required=False, default=0.3,
-#                      help="Maximum MAF allowed for a palindromic variant")
-# parser2.add_argument(
-#     'X-Api-Token', location='headers', required=False, default='null',
-#     help='Public datasets can be queried without any authentication, but some studies are only accessible by specific users. To authenticate we use Google OAuth2.0 access tokens. The easiest way to obtain an access token is through the [TwoSampleMR R](https://mrcieu.github.io/TwoSampleMR/#authentication) package using the `get_mrbase_access_token()` function.')
-
-
-# @api.route('/')
-# @api.doc(
-#     description="""
-# Get specific SNP associations for specifc GWAS datasets. Note the payload can be passed to curl via json using:
-
-# ```
-# -X POST -d '
-# {
-#     'id': ['2','1001']
-# }
-# '
-# ```
-
-# """
-# )
-# class AssocPost(Resource):
-#     @api.expect(parser2)
-#     def post(self):
-#         args = parser2.parse_args()
-
-#         if (len(args['id']) == 0):
-#             abort(405)
-
-#         if (len(args['rsid']) == 0):
-#             abort(405)
-
-#         try:
-#             user_email = get_user_email(request.headers.get('X-Api-Token'))
-#             out = get_assoc(user_email, args['rsid'], args['id'], args['proxies'], args['r2'], args['align_alleles'],
-#                             args['palindromes'], args['maf_threshold'])
-#         except Exception as e:
-#             logger.error("Could not obtain SNP association: {}".format(e))
-#             abort(503)
-#         return out, 200
