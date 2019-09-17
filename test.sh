c9579b7682374a47837c523a775660933c784e7d
Matt Lyon <ml18692@bristol.ac.uk>
2019-09-17 16:30:05 +0100

    updated args

--- test.sh
+++ test.sh
@@ -11,7 +11,11 @@ mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p'M1st3rbase!' -
 mysql -h ieu-db-interface.epi.bris.ac.uk -P 13306 -u mrbaseapp -p'M1st3rbase!' -B -N -e "select * from memberships" mrbase | sed 's/\\n//g' > /tmp/memberships.tsv
 
 # import data to graph
-docker exec -it mr-base-api-v2-test python populate_db/map_from_csv.py
+docker exec -it mr-base-api-v2-test python populate_db/map_from_csv.py \
+--study /tmp/study_e.tsv \
+--groups /tmp/groups.tsv \
+--permissions_e /tmp/permissions_e.tsv \
+--memberships /tmp/memberships.tsv
 
 # run unit API tests
 docker exec -e MRB_TOKEN="$token" -it mr-base-api-v2-test pytest -v apis/ --url http://localhost
