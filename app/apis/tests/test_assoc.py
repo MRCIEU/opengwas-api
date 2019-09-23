db9e41ad93daf03e14e7d0156bd2be034b645bba
Matthew Lyon <ml@vpn-user-247-101.nomadic.bris.ac.uk>
2019-09-23 21:47:06 +0100

    fixed test

--- app/apis/tests/test_assoc.py
+++ app/apis/tests/test_assoc.py
@@ -7,33 +7,33 @@ token = get_mrbase_access_token()
 ## GET
 
 def test_assoc_get1(url):
-    r = requests.get(url + "/associations/2/rs234")
+    r = requests.get(url + "/associations/IEU-a-2/rs234")
     assert r.status_code == 200 and len(r.json()) == 1
 
 
 def test_assoc_get2(url):
-    r = requests.get(url + "/associations/300/rs4988235")
+    r = requests.get(url + "/associations/IEU-a-300/rs4988235")
     assert r.status_code == 200 and len(r.json()) == 1
 
 
 def test_assoc_get3(url):
-    r = requests.get(url + "/associations/2,987/rs234")
+    r = requests.get(url + "/associations/IEU-a-2,IEU-a-987/rs234")
     assert r.status_code == 200 and len(r.json()) == 1
 
 
 def test_assoc_get4(url):
-    r = requests.get(url + "/associations/2,987,7/rs234")
+    r = requests.get(url + "/associations/IEU-a-2,IEU-a-987,7/rs234")
     assert r.status_code == 200 and len(r.json()) == 2
 
 
 def test_assoc_get5(url):
-    r = requests.get(url + "/associations/2,987,7/rs234,rs123")
+    r = requests.get(url + "/associations/IEU-a-2,IEU-a-987,IEU-a-7/rs234,rs123")
     assert r.status_code == 200 and len(r.json()) == 4
 
 
 def test_assoc_get6(url):
     headers = {'X-API-TOKEN': token}
-    r = requests.get(url + "/associations/2,987,7/rs234,rs123", headers=headers)
+    r = requests.get(url + "/associations/IEU-a-2,IEU-a-987,IEU-a-7/rs234,rs123", headers=headers)
     assert r.status_code == 200 and len(r.json()) == 6
 
 
@@ -41,41 +41,41 @@ def test_assoc_get6(url):
 
 # Should return json entries for each study
 def test_assoc_post1(url):
-    payload = {'id': ['2', '7', '987'], 'rsid': ['rs234', 'rs123']}
+    payload = {'id': ['IEU-a-2', 'IEU-a-7', 'IEU-a-987'], 'rsid': ['rs234', 'rs123']}
     r = requests.post(url + "/associations/", data=payload)
     assert r.status_code == 200 and len(r.json()) == 4
 
 
 def test_assoc_post2(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['2', '7', '987'], 'rsid': ['rs234', 'rs123']}
+    payload = {'id': ['IEU-a-2', 'IEU-a-7', 'IEU-a-987'], 'rsid': ['rs234', 'rs123']}
     r = requests.post(url + "/associations/", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) == 6
 
 
 def test_assoc_post3(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['1', '2'], 'rsid': ['rs6689306']}
+    payload = {'id': ['IEU-a-1', 'IEU-a-2'], 'rsid': ['rs6689306']}
     r = requests.post(url + "/associations/", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) == 0
 
 
 def test_assoc_post4(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['1', '2'], 'rsid': ['rs6689306'], 'proxies': 1}
+    payload = {'id': ['IEU-a-1', 'IEU-a-2'], 'rsid': ['rs6689306'], 'proxies': 1}
     r = requests.post(url + "/associations/", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) == 2
 
 
 def test_assoc_post5(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['1', '2'], 'rsid': ['rs234', 'rs123', 'rs6689306']}
+    payload = {'id': ['IEU-a-1', 'IEU-a-2'], 'rsid': ['rs234', 'rs123', 'rs6689306']}
     r = requests.post(url + "/associations/", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) == 4
 
 
 def test_assoc_post6(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['1', '2'], 'rsid': ['rs234', 'rs123', 'rs6689306'], 'proxies': 1}
+    payload = {'id': ['IEU-a-1', 'IEU-a-2'], 'rsid': ['rs234', 'rs123', 'rs6689306'], 'proxies': 1}
     r = requests.post(url + "/associations/", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) == 6
--- app/apis/tests/test_db_content.py
+++ app/apis/tests/test_db_content.py
@@ -6,21 +6,21 @@ import flask
 def test_get_permitted_studies():
     app = flask.Flask(__name__)
     with app.app_context():
-        assert len(get_permitted_studies(None, ['300'])) == 1
-        assert len(get_permitted_studies('ml18692@bristol.ac.uk', ['300'])) == 1
+        assert len(get_permitted_studies(None, ['IEU-a-300'])) == 1
+        assert len(get_permitted_studies('ml18692@bristol.ac.uk', ['IEU-a-300'])) == 1
 
 
 def test_get_assoc():
     app = flask.Flask(__name__)
     with app.app_context():
-        assert len(get_assoc(None, ['rs4747841'], ['300'], 1, 0.8, 1, 1, 0.3)) == 1
-        assert len(get_assoc('ml18692@bristol.ac.uk', ['rs4747841'], ['300'], 1, 0.8, 1, 1, 0.3)) == 1
+        assert len(get_assoc(None, ['rs4747841'], ['IEU-a-300'], 1, 0.8, 1, 1, 0.3)) == 1
+        assert len(get_assoc('ml18692@bristol.ac.uk', ['rs4747841'], ['IEU-a-300'], 1, 0.8, 1, 1, 0.3)) == 1
 
 
 def test_query_summary_stats():
     app = flask.Flask(__name__)
     with app.app_context():
-        assert len(query_summary_stats(None, ['rs4747841'], ['300'])) == 1
-        assert len(query_summary_stats('ml18692@bristol.ac.uk', ['rs4747841'], ['300'])) == 1
-        assert len(query_summary_stats('ml18692@bristol.ac.uk', ['rs4747841'], ['300'])) == 1
+        assert len(query_summary_stats(None, ['rs4747841'], ['IEU-a-300'])) == 1
+        assert len(query_summary_stats('ml18692@bristol.ac.uk', ['rs4747841'], ['IEU-a-300'])) == 1
+        assert len(query_summary_stats('ml18692@bristol.ac.uk', ['rs4747841'], ['IEU-a-300'])) == 1
         assert len(query_summary_stats(None, ['rs4747841'], 'snp_lookup')) > 20000
