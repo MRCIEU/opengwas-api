93527e0b0b1400163afc5b358ce088f5a8050e1b
Gibran Hemani <explodecomputer@gmail.com>
2019-12-05 15:07:00 +0000

    updated index page

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import flask
+from os import path, walk
 from resources.globals import Globals
 from apis import api
 from resources.logging_middleware import LoggerMiddleWare
@@ -63,4 +64,12 @@ app.teardown_appcontext(Neo4j.close_db)
 api.init_app(app)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=Globals.app_config['flask']['port'])
+    extra_dirs = ['templates','static']
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in walk(extra_dir):
+            for filename in files:
+                filename = path.join(dirname, filename)
+                if path.isfile(filename):
+                    extra_files.append(filename)
+    app.run(host='0.0.0.0', port=Globals.app_config['flask']['port'], extra_files=extra_files)
