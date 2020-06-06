8357994cfa7029f4706a8a1a11e12f9e4ec79009
Gibran Hemani <explodecomputer@gmail.com>
2020-06-06 15:07:31 +0100

    various updates to edit endpoint
    - added md5 check during file upload
    - added coverage and ontology mappings

--- app/schemas/gwas_info_node_schema.py
+++ app/schemas/gwas_info_node_schema.py
@@ -74,9 +74,12 @@ valid_study_designs = {
     "Other",
     "Unknown"
 }
-valid_imputation_panels = {'no imputed genotypes', '1000 Genomes', 'HapMap', 'HRC', 'UK10K', 'other'}
+valid_imputation_panels = {'not imputed', 'HapMap2', 'HapMap3', '1000 Genomes', 'UK10K', 'HRC', 'TOPMed', 'other'}
 valid_genome_build = {'HG19/GRCh37'}
 
+valid_coverage = {
+    "whole genome", "partial", "exome"
+}
 
 def check_study_year(data):
     if data < 2000 or data > 2050:
@@ -141,63 +144,71 @@ def check_id_is_valid_filename(data):
 
 
 class GwasInfoNodeSchema(FRPMSchema):
-    id = fields.Str(required=True, allow_none=False, description="GWAS study identifier",
-                    validate=check_id_is_valid_filename)
+    id = fields.Str(required=True, allow_none=False, 
+        description="GWAS study identifier",
+        validate=check_id_is_valid_filename)
     pmid = fields.Int(required=False, allow_none=True,
-                      description="Pubmed identifier. Leave blank for unpublished studies.")
+        description="Pubmed identifier. Leave blank for unpublished studies.")
     doi = fields.Str(required=False, allow_none=True,
-                     description="DOI. Leave blank for unpublished studies.", validate=check_doi)
+        description="DOI. Leave blank for unpublished studies.",
+        validate=check_doi)
     year = fields.Int(required=False, validate=check_study_year, allow_none=True,
-                      description="What year was this GWAS published?")
-    mr = fields.Int(required=False, allow_none=True, validate=check_mr_is_0_or_1,
-                    description="Is the study suitable for MR studies?", choices=(0, 1))
+        description="What year was this GWAS published?")
+    mr = fields.Int(required=False, allow_none=True, 
+        validate=check_mr_is_0_or_1,
+        description="Is the study suitable for MR studies?", choices=(0, 1))
     note = fields.Str(required=False, allow_none=True,
-                      description="Is there any other information you would like to provide us about your GWAS?")
+        description="Is there any other information you would like to provide us about your GWAS?")
     trait = fields.Str(required=True, allow_none=False,
-                       description="Avoid acronyms; don't include other information in the trait name (e.g. don't include array name, whether restricted to males or females or whether adjusted or unadjusted for covariates)")
+        description="Avoid acronyms; don't include other information in the trait name (e.g. don't include array name, whether restricted to males or females or whether adjusted or unadjusted for covariates)")
     #trait_description = fields.Str(required=False, validate=check_trait_description, allow_none=True,
-    #                               choices=sorted(list(valid_trait_descriptions)),
-    #                               description="Describe the distribution of your phenotype")
+        # choices=sorted(list(valid_trait_descriptions)),
+        # description="Describe the distribution of your phenotype")
     category = fields.Str(required=True, validate=check_category_is_valid, allow_none=False,
-                          description="Is your phenotype a binary disease phenotype or a non-disease phenotype",
-                          choices=sorted(list(valid_categories)))
-    subcategory = fields.Str(required=True, validate=check_subcategory_is_valid, allow_none=False,
-                             description="Select the option that best describes your phenotype.",
-                             choices=sorted(list(valid_trait_subcategories)))
+        description="Is your phenotype a binary disease phenotype or a non-disease phenotype",
+        choices=sorted(list(valid_categories)))
+    subcategory = fields.Str(required=True, allow_none=False,
+        validate=check_subcategory_is_valid,
+        description="Select the option that best describes your phenotype.",
+        choices=sorted(list(valid_trait_subcategories)))
+    ontology = fields.Str(required=False, allow_none=True,
+        description="Ontology mapping, semi-colon separated, e.g. 'MONDO:0003274;EFO:0000311'")
     population = fields.Str(required=True, validate=check_population_is_valid, allow_none=False,
-                            description="Describe the geographic origins of your population",
-                            choices=sorted(list(valid_populations)))
+        description="Describe the geographic origins of your population",
+        choices=sorted(list(valid_populations)))
     sex = fields.Str(required=True, validate=check_sex_is_valid, allow_none=False,
-                     description="Indicate whether males or females are included in your study",
-                     choices=sorted(list(valid_sex)))
+        description="Indicate whether males or females are included in your study",
+        choices=sorted(list(valid_sex)))
     ncase = fields.Int(required=False, allow_none=True,
-                       description="Provide number of cases in your study (if applicable)")
+        description="Provide number of cases in your study (if applicable)")
     ncontrol = fields.Int(required=False, allow_none=True,
-                          description="Provide number of controls in your study (if applicable)")
+        description="Provide number of controls in your study (if applicable)")
     sample_size = fields.Int(required=False, allow_none=True, description="Provide the sample size of your study")
     nsnp = fields.Int(required=False, allow_none=True,
-                      description="How many SNPs are in your results file that you are uploading?")
+        description="How many SNPs are in your results file that you are uploading?")
     unit = fields.Str(required=False, allow_none=True,
-                      description="How do you interpret a 1-unit change in the phenotype? eg log odds ratio, mmol/L, SD units?")
+        description="How do you interpret a 1-unit change in the phenotype? eg log odds ratio, mmol/L, SD units?")
     sd = fields.Float(required=False, allow_none=True,
-                      description="What is the standard deviation of the sample mean of the phenotype?")
+        description="What is the standard deviation of the sample mean of the phenotype?")
     priority = fields.Int(required=True, allow_none=False)
     author = fields.Str(required=False, allow_none=True,
-                        description="Provide the last name of the first author of your study")
+        description="Provide the last name of the first author of your study")
     consortium = fields.Str(required=False, allow_none=True,
-                            description="What is the name of your study or consortium (if applicable)?")
+        description="What is the name of your study or consortium (if applicable)?")
     study_design = fields.Str(required=False, validate=check_study_design_is_valid, allow_none=True,
-                              description="Which best describes the design of your study",
-                              choices=sorted(list(valid_study_designs)))
+        description="Which best describes the design of your study",
+        choices=sorted(list(valid_study_designs)))
     covariates = fields.Str(required=False, allow_none=True,
-                            description="Describe the covariates included in your regression model")
+        description="Describe the covariates included in your regression model")
     beta_transformation = fields.Str(required=False, allow_none=True,
-                                     description="Describe transformations applied to your phenotype (e.g. inverse rank normal, Z transformations, etc)")
+        description="Describe transformations applied to your phenotype (e.g. inverse rank normal, Z transformations, etc)")
     imputation_panel = fields.Str(required=False, validate=check_imputation_panel_is_valid, allow_none=True,
-                                  description="Select the imputation panel used in your study",
-                                  choices=sorted(list(valid_imputation_panels)))
+        description="Select the imputation panel used in your study",
+        choices=sorted(list(valid_imputation_panels)))
     build = fields.Str(required=False, validate=check_genome_build_is_valid, allow_none=True,
-                       description="Select the genome build for your study", choices=sorted(list(valid_genome_build)))
-    md5 = fields.Str(required=False, allow_none=True)
+        description="Select the genome build for your study", choices=sorted(list(valid_genome_build)))
+    coverage = fields.Str(required=False, allow_none=True,
+        description="Level of genome coverage",
+        choices=list(valid_coverage))
     qc_prior_to_upload = fields.Str(required=False, allow_none=True,
                                     description="Detail any QC or filtering steps taken prior to data upload")
