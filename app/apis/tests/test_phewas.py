42969fd3ba39cf6a02c14bd107e0a082d3df19c4
Gibran Hemani <explodecomputer@gmail.com>
2021-01-22 16:27:38 +0000

    added more tests for afl2 and reduced threshold for phewas tests

--- app/apis/tests/test_phewas.py
+++ app/apis/tests/test_phewas.py
@@ -11,7 +11,7 @@ def test_phewas_get1(url):
     assert r.status_code == 200 and len(r.json()) > 5
 
 def test_phewas_get2(url):
-    r = requests.get(url + "/phewas/rs234/1")
+    r = requests.get(url + "/phewas/rs234/0.01")
     assert r.status_code == 200 and len(r.json()) > 100
 
 
@@ -25,19 +25,19 @@ def test_phewas_post1(url):
 
 def test_phewas_post2(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'variant': 'rs234', 'pval': 1}
+    payload = {'variant': 'rs234', 'pval': 0.01}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 100
 
-def test_phewas_post2(url):
+def test_phewas_post3(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'variant': '7:105561135', 'pval': 1}
+    payload = {'variant': '7:105561135', 'pval': 0.01}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 100
 
-def test_phewas_post2(url):
+def test_phewas_post4(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'variant': '7:105561135-105563135', 'pval': 1}
+    payload = {'variant': '7:105561135-105563135', 'pval': 0.01}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 100
 
