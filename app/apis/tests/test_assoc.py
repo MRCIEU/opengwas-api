5e0e9c9a34305b6f5d7829f508fd082291d6398b
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2023-11-08 20:58:05 +0000

    fix: Update test scripts by replacing ieu-b-5007 with ieu-a-998

--- app/apis/tests/test_assoc.py
+++ app/apis/tests/test_assoc.py
@@ -5,6 +5,7 @@ token = get_mrbase_access_token()
 
 ## GET
 
+
 def test_assoc_get1(url):
     r = requests.get(url + "/associations/ieu-a-2/rs234")
     assert r.status_code == 200 and len(r.json()) == 1
@@ -16,23 +17,23 @@ def test_assoc_get2(url):
 
 
 def test_assoc_get3(url):
-    r = requests.get(url + "/associations/ieu-a-2,ieu-b-5007/rs234")
+    r = requests.get(url + "/associations/ieu-a-2,ieu-a-998/rs234")
     assert r.status_code == 200 and len(r.json()) == 1
 
 
 def test_assoc_get4(url):
-    r = requests.get(url + "/associations/ieu-a-2,ieu-b-5007,ieu-a-7/rs234")
+    r = requests.get(url + "/associations/ieu-a-2,ieu-a-998,ieu-a-7/rs234")
     assert r.status_code == 200 and len(r.json()) == 2
 
 
 def test_assoc_get5(url):
-    r = requests.get(url + "/associations/ieu-a-2,ieu-b-5007,ieu-a-7/rs234,rs123")
+    r = requests.get(url + "/associations/ieu-a-2,ieu-a-998,ieu-a-7/rs234,rs123")
     assert r.status_code == 200 and len(r.json()) == 4
 
 
 def test_assoc_get6(url):
     headers = {'X-API-TOKEN': token}
-    r = requests.get(url + "/associations/ieu-a-2,ieu-b-5007,ieu-a-7/rs234,rs123", headers=headers)
+    r = requests.get(url + "/associations/ieu-a-2,ieu-a-998,ieu-a-7/rs234,rs123", headers=headers)
     assert r.status_code == 200 and len(r.json()) >= 6
 
 
@@ -40,14 +41,14 @@ def test_assoc_get6(url):
 
 # Should return json entries for each study
 def test_assoc_post1(url):
-    payload = {'id': ['ieu-a-2', 'ieu-a-7', 'ieu-b-5007'], 'variant': ['rs234', 'rs123']}
+    payload = {'id': ['ieu-a-2', 'ieu-a-7', 'ieu-a-998'], 'variant': ['rs234', 'rs123']}
     r = requests.post(url + "/associations", data=payload)
     assert r.status_code == 200 and len(r.json()) == 4
 
 
 def test_assoc_post2(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['ieu-a-2', 'ieu-a-7', 'ieu-b-5007'], 'variant': ['rs234', 'rs123']}
+    payload = {'id': ['ieu-a-2', 'ieu-a-7', 'ieu-a-998'], 'variant': ['rs234', 'rs123']}
     r = requests.post(url + "/associations", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) >= 6
 
@@ -81,6 +82,7 @@ def test_assoc_post6(url):
 
 # chrpos
 
+
 def test_chrpos1(url):
     headers = {'X-API-TOKEN': token}
     payload = {'id': ['ieu-a-2'], 'variant': ['7:105561135'], 'proxies': 0}
@@ -89,6 +91,7 @@ def test_chrpos1(url):
     r2 = requests.post(url + "/associations", data=payload, headers=headers).json()
 
     assert r1[0]['rsid'] == r2[0]['rsid']
+
 
 def test_chrpos2(url):
     headers = {'X-API-TOKEN': token}
@@ -99,10 +102,10 @@ def test_chrpos2(url):
 
     assert len(r1) == 2
 
+
 def test_chrpos3(url):
     headers = {'X-API-TOKEN': token}
     payload = {'id': ['ieu-a-2'], 'variant': ['7:105561135-105571135', 'rs1205'], 'proxies': 0}
     r1 = requests.post(url + "/associations", data=payload, headers=headers).json()
     assert 'rs1205' in [x['rsid'] for x in r1]
     assert len(r1) > 1
-
--- app/apis/tests/test_tophits.py
+++ app/apis/tests/test_tophits.py
@@ -5,15 +5,16 @@ token = get_mrbase_access_token()
 
 
 def test_tophits_post1(url):
-    payload = {'id': ['ieu-a-2', 'ieu-b-5007']}
+    payload = {'id': ['ieu-a-2', 'ieu-a-998']}
     r = requests.post(url + "/tophits", data=payload)
     print(r.json())
     assert r.status_code == 200
     assert len(r.json()) < 3000
     assert len(set([x.get('id') for x in r.json()])) == 1
 
+
 def test_tophits_post1a(url):
-    payload = {'id': ['ieu-a-2', 'ieu-b-5007'], 'preclumped': 0, 'clump': 1}
+    payload = {'id': ['ieu-a-2', 'ieu-a-998'], 'preclumped': 0, 'clump': 1}
     r = requests.post(url + "/tophits", data=payload)
     print(r.json())
     assert r.status_code == 200
@@ -23,7 +24,7 @@ def test_tophits_post1a(url):
 
 def test_tophits_post2(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['ieu-a-2', 'ieu-b-5007'], 'preclumped': 0, 'clump': 0}
+    payload = {'id': ['ieu-a-2', 'ieu-a-998'], 'preclumped': 0, 'clump': 0}
     r = requests.post(url + "/tophits", data=payload, headers=headers)
     assert r.status_code == 200
     assert len(r.json()) > 2000
@@ -31,7 +32,7 @@ def test_tophits_post2(url):
 
 
 def test_tophits_post3(url):
-    payload = {'id': ['ieu-a-2', 'ieu-b-5007'], 'preclumped': 0, 'clump': 1}
+    payload = {'id': ['ieu-a-2', 'ieu-a-998'], 'preclumped': 0, 'clump': 1}
     r = requests.post(url + "/tophits", data=payload)
     assert r.status_code == 200
     assert len(r.json()) < 80
@@ -40,7 +41,7 @@ def test_tophits_post3(url):
 
 def test_tophits_post4(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['ieu-a-2', 'ieu-b-5007'], 'preclumped': 0, 'clump': 1}
+    payload = {'id': ['ieu-a-2', 'ieu-a-998'], 'preclumped': 0, 'clump': 1}
     r = requests.post(url + "/tophits", data=payload, headers=headers)
     assert r.status_code == 200
     assert len(r.json()) > 70
@@ -49,7 +50,7 @@ def test_tophits_post4(url):
 
 def test_tophits_post5(url):
     headers = {'X-API-TOKEN': token}
-    payload = {'id': ['ieu-a-2', 'ieu-b-5007'], 'preclumped': 0}
+    payload = {'id': ['ieu-a-2', 'ieu-a-998'], 'preclumped': 0}
     r = requests.post(url + "/tophits", data=payload, headers=headers)
     assert r.status_code == 200
     assert len(r.json()) > 70
