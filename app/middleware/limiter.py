9a3f9ae1a5583e2fbeb5c9bc33624402ebcc8437
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2023-11-13 22:02:21 +0000

    feat: Add flask-limiter

---
+++ app/middleware/limiter.py
@@ -0,0 +1,10 @@
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from resources.globals import Globals
+
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    strategy='fixed-window-elastic-expiry',
+    storage_uri='redis://:' + Globals.app_config['redis']['pass'] + '@' + Globals.app_config['redis']['host'] + ':' + Globals.app_config['redis']['port'],
+)
