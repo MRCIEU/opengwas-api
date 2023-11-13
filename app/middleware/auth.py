9d34c339f4df18cfbda0cccbfae474a40da82ddb
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2023-11-13 22:04:32 +0000

    feat: Add JWT user auth
    - Send verification link via email
    - Login using the link
    - Registration or add user's names using the link
    - Validate JWT

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,6 +1,9 @@
 from flask import request
 from functools import wraps
-from apis.users import validate_jwt
+from queries.cql_queries import get_user_by_email
+from resources.globals import Globals
+import jwt
+from werkzeug.exceptions import BadRequest
 
 
 def jwt_required():
@@ -11,3 +14,25 @@ def jwt_required():
             return f(*args, **kwargs)
         return _decorator
     return decorator
+
+
+def validate_jwt(token):
+    try:
+        payload = jwt.decode(token, Globals.JWT_KEY, algorithms=['HS256'])
+    except jwt.exceptions.InvalidSignatureError:
+        raise BadRequest("Invalid JWT signature.")
+    except jwt.exceptions.DecodeError:
+        raise BadRequest("Invalid JWT header or payload.")
+
+    user = get_user_by_email(payload['uid'])
+    if user is None:
+        raise BadRequest('User does not exist or has been deactivated.')
+    user = user.data()['u']
+    if 'jwt_timestamp' not in user:
+        raise BadRequest('Invalid JWT.')
+
+    if payload['timestamp'] != user['jwt_timestamp']:
+        raise BadRequest("Please generate a new token.")
+
+    # TODO: add counter
+    return user
