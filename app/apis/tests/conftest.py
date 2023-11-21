8504ca45f43b9dc77ec14db20523f327431a9886
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2023-11-21 17:20:50 +0000

    test: Disable tests related to dataset upload and release

--- app/apis/tests/conftest.py
+++ app/apis/tests/conftest.py
@@ -4,14 +4,17 @@ import pytest
 from resources.globals import Globals
 
 
+collect_ignore = ["test_edit.py", "test_quality_control.py"]
+
+
 def pytest_configure():
     pytest.shared_variable = {}
 
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--url", action="store", default="http://0.0.0.0:8019",
-        help="URL to look for the deployed API. Default is local test http://0.0.0.0:8019"
+        "--url", action="store", default="http://0.0.0.0:8019/api",
+        help="URL to look for the deployed API. Default is local test http://0.0.0.0:8019/api"
     )
 
 
