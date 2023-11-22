d9ced79359be7adf339e7b20c8d3232f97f60af7
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2023-11-22 11:36:10 +0000

    test: Remove JWT from API tests

--- app/apis/tests/test_users.py
+++
@@ -1,8 +0,0 @@
-import requests
-import pytest
-
-
-def test_signin_by_jwt(url, headers):
-    headers = {'X-Api-JWT': pytest.shared_variable['users_jwt_token']}
-    r = requests.get(url + "/users/signin_by_jwt", headers=headers)
-    assert r.status_code == 200 and r.json().get('uid')
