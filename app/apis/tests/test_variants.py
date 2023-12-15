1891a27b466e4b2af98aa5c695cece3c908c1bae
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2023-12-15 12:34:22 +0000

    test: Update test_variants.py

--- app/apis/tests/test_variants.py
+++ app/apis/tests/test_variants.py
@@ -26,12 +26,19 @@ def test_chrpos_get0(url, headers):
 
 def test_chrpos_get1(url, headers):
     r = requests.get(url + "/variants/chrpos/7:105561135", headers=headers)
-    assert r.status_code == 200 and len(r.json()) == 1
+    o = r.json()
+    assert r.status_code == 200 and len(o) == 1
+    assert len(o[0]) > 12
 
 
 def test_chrpos_get2(url, headers):
     r = requests.get(url + "/variants/chrpos/7:105561135?radius=1000", headers=headers)
     assert r.status_code == 200 and len(r.json()[0]) > 1
+
+
+def test_chrpos_get3(url, headers):
+    r = requests.get(url + "/variants/chrpos/7:105561135-105563135,10:44865737", headers=headers)
+    assert r.status_code == 200 and len(r.json()) == 2
 
 
 def test_gene_get0(url, headers):
@@ -98,13 +105,13 @@ def test_afl2_get3(url, headers):
 
 
 def test_afl2_post0(url, headers):
-    payload = {'chrpos': ['7:105561135', '10:44865737'], 'rsid': ['rs123']}
-    r = requests.post(url + "/variants/afl2", data=payload, headers=headers)
-    assert r.status_code == 200 and len(r.json()) == 3
-
-
-def test_afl2_post1(url, headers):
     del headers['Authorization']
     payload = {'chrpos': ['7:105561135', '10:44865737'], 'rsid': ['rs123']}
     r = requests.post(url + "/variants/afl2", data=payload, headers=headers)
     assert r.status_code == 401
+
+
+def test_afl2_post1(url, headers):
+    payload = {'chrpos': ['7:105561135', '10:44865737'], 'rsid': ['rs123']}
+    r = requests.post(url + "/variants/afl2", data=payload, headers=headers)
+    assert r.status_code == 200 and len(r.json()) == 3
