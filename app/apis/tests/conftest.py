60faba5861cf889a59ead6d6786dfa3cb12d6d5a
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-05-04 21:28:19 +0100

    test: Add test for user/

--- app/apis/tests/conftest.py
+++ app/apis/tests/conftest.py
@@ -27,7 +27,10 @@ def url(request):
 
 @pytest.fixture(scope="session")
 def headers():
+    # For API tests here, X-TEST-NO-RATE-LIMIT-KEY is provided to bypass flask-limiter while JWT is for authentication
+    # For tests from clients e.g. ieugwasr, X-TEST-MODE-KEY is for bypassing flask-limiter and also for authentication, and there is no need to provide JWT. However, when provided, JWT will be used for authentication
+    # See app/middleware/auth.py and app/apis/tests/test_user.py
     return {
-        'X-TEST-MODE-KEY': Globals.app_config['test']['test_mode_key'],
+        'X-TEST-NO-RATE-LIMIT-KEY': Globals.app_config['test']['no_rate_limit_key'],
         'Authorization': 'Bearer ' + get_token()
     }
---
+++ app/apis/tests/test_user.py
@@ -0,0 +1,20 @@
+import requests
+
+from resources.globals import Globals
+
+
+# Test whether static token is working and whether flask-limiter is disabled
+def test_user_static_token(url, headers):
+    headers = headers.copy()
+    del headers['X-TEST-NO-RATE-LIMIT-KEY']
+    headers['X-TEST-MODE-KEY'] = Globals.app_config['test']['static_token']
+    r = requests.get(url + "/user", headers=headers)
+    assert r.status_code == 200 and r.json()['user']['uid'] == Globals.app_config['test']['uid']
+    assert 'X-Allowance-Limit' not in r.headers
+
+
+# Test whether JWT is working and whether flask-limiter is disabled
+def test_user_jwt(url, headers):
+    r = requests.get(url + "/user", headers=headers)
+    assert r.status_code == 200 and r.json()['user']['uid'] == Globals.app_config['test']['uid']
+    assert 'X-Allowance-Limit' not in r.headers
