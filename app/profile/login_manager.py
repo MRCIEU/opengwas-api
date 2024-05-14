709b6513c27f8a1c39792a6cff08cba0f8b2ea85
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-05-14 09:39:37 +0100

    feat: Add role_required() decorator

--- app/profile/login_manager.py
+++ app/profile/login_manager.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from flask_login import LoginManager, login_user, current_user, logout_user
 
 from queries.user_node import User
@@ -26,3 +27,14 @@ def signin_user(user, source):
 
 def sign_out_user():
     logout_user()
+
+
+def role_required(role):
+    def _decorator(f):
+        @wraps(f)
+        def _check_role(*args, **kwargs):
+            if 'role' not in current_user or role not in current_user['role']:
+                return login_manager.unauthorized()
+            return f(*args, **kwargs)
+        return _check_role
+    return _decorator
