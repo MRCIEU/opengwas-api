dd77ab54551736f31935d8a684b6095b0a055c3d
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-05-23 21:13:15 +0100

    test: Add test for /phewas/fast

--- app/apis/tests/test_phewas.py
+++ app/apis/tests/test_phewas.py
@@ -49,3 +49,35 @@ def test_phewas_post4(url, headers):
     payload = {'variant': '7:105561135-105563135', 'pval': 0.01}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 400
+
+
+def test_phewas_fast_post0(url, headers):
+    headers = headers.copy()
+    del headers['Authorization']
+    payload = {'variant': 'rs234', 'pval': 0.001}
+    r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
+    assert r.status_code == 401
+
+
+def test_phewas_fast_post1(url, headers):
+    payload = {'variant': 'rs234', 'pval': 0.001}
+    r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
+    assert r.status_code == 200 and len(r.json()) > 20
+
+
+def test_phewas_fast_post2(url, headers):
+    payload = {'variant': 'rs234', 'pval': 0.01}
+    r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
+    assert r.status_code == 200 and len(r.json()) > 200
+
+
+def test_phewas_fast_post3(url, headers):
+    payload = {'variant': '7:105561135', 'pval': 0.01}
+    r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
+    assert r.status_code == 200 and len(r.json()) > 200
+
+
+def test_phewas_fast_post4(url, headers):
+    payload = {'variant': '7:105561135-105563135', 'pval': 0.01}
+    r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
+    assert r.status_code == 200 and len(r.json()) > 400
