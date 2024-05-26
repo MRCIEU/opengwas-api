13d107a461c1e7a908e510bde6db11f121c4e759
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-05-26 19:20:46 +0100

    test: Remove JWT from test_user_static_token()

--- app/apis/tests/test_user.py
+++ app/apis/tests/test_user.py
@@ -4,10 +4,10 @@ from resources.globals import Globals
 
 
 # Test whether static token is working and whether flask-limiter is disabled
-def test_user_static_token(url, headers):
-    headers = headers.copy()
-    del headers['X-TEST-NO-RATE-LIMIT-KEY']
-    headers['X-TEST-MODE-KEY'] = Globals.app_config['test']['static_token']
+def test_user_static_token(url):
+    headers = {
+        'X-TEST-MODE-KEY': Globals.app_config['test']['static_token']
+    }
     r = requests.get(url + "/user", headers=headers)
     assert r.status_code == 200 and r.json()['user']['uid'] == Globals.app_config['test']['uid']
     assert 'X-Allowance-Limit' not in r.headers
