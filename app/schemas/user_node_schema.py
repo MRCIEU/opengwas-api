ed2ad28d10ff19e7c4c3de6c12e5043045369fd7
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-06-28 19:25:33 +0100

    feat: Remove 'admin' field from User node

--- app/schemas/user_node_schema.py
+++ app/schemas/user_node_schema.py
@@ -5,7 +5,6 @@ from schemas.frpm_schema import FRPMSchema
 class UserNodeSchema(FRPMSchema):
     uid = fields.Str(required=True, validate=validate.Email(error='Not a valid email address'),
                      metadata={"description": "Email address of user."})
-    admin = fields.Bool(required=False, metadata={"description": "Is the user an admin?"})
     role = fields.Str(required=False, validate=validate.OneOf(['admin']), metadata={"description": "Role of user"})
     first_name = fields.Str(required=True, allow_none=False, validate=validate.Length(min=1, max=40),
                             metadata={"description": "First name"})
