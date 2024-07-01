a09400dd28fd818237d690061ae330f25f623498
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-07-01 19:56:46 +0100

    test: Skip tests for legacy /phewas

--- app/apis/tests/test_phewas.py
+++ app/apis/tests/test_phewas.py
@@ -1,7 +1,9 @@
+import pytest
 import requests
 
 
 # Anonymous
+@pytest.mark.skip
 def test_phewas_get0(url, headers):
     headers = headers.copy()
     del headers['Authorization']
@@ -9,11 +11,13 @@ def test_phewas_get0(url, headers):
     assert r.status_code == 401
 
 
+@pytest.mark.skip
 def test_phewas_get1(url, headers):
     r = requests.get(url + "/phewas/rs234/0.001", headers=headers)
     assert r.status_code == 200 and len(r.json()) > 20
 
 
+@pytest.mark.skip
 def test_phewas_get2(url, headers):
     r = requests.get(url + "/phewas/rs234/0.01", headers=headers)
     assert r.status_code == 200 and len(r.json()) > 200
@@ -51,6 +55,7 @@ def test_phewas_post4(url, headers):
     assert r.status_code == 200 and len(r.json()) > 400
 
 
+@pytest.mark.skip
 def test_phewas_fast_post0(url, headers):
     headers = headers.copy()
     del headers['Authorization']
@@ -59,24 +64,28 @@ def test_phewas_fast_post0(url, headers):
     assert r.status_code == 401
 
 
+@pytest.mark.skip
 def test_phewas_fast_post1(url, headers):
     payload = {'variant': 'rs234', 'pval': 0.001}
     r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 20
 
 
+@pytest.mark.skip
 def test_phewas_fast_post2(url, headers):
     payload = {'variant': 'rs234', 'pval': 0.01}
     r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 200
 
 
+@pytest.mark.skip
 def test_phewas_fast_post3(url, headers):
     payload = {'variant': '7:105561135', 'pval': 0.01}
     r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 200
 
 
+@pytest.mark.skip
 def test_phewas_fast_post4(url, headers):
     payload = {'variant': '7:105561135-105563135', 'pval': 0.01}
     r = requests.post(url + "/phewas/fast", data=payload, headers=headers)
