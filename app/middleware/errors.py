2b9e6692b1ca3b89aa7d7818acab1880b43d5a94
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-07-24 09:52:46 +0100

    feat: Update error message when the token is missing

--- app/middleware/errors.py
+++ app/middleware/errors.py
@@ -6,7 +6,7 @@ from . import limiter
 def raise_error(key):
     if key == 'MISSING_TOKEN':
         with limiter.limiter.shared_limit(limit_value="100 per 1 minutes", scope='allowance_of_test_mode'):  # per IP
-            raise Unauthorized("From 1st May 2024 you must provide a token (JWT) alongside most of your requests. Read more at https://api.opengwas.io/ and also check for the latest version at https://mrcieu.github.io/ieugwasr/")
+            raise Unauthorized("ERROR - Go to https://api.opengwas.io/ - From 1st May 2024 you must provide a token (JWT) alongside most of your requests. Read more at https://api.opengwas.io/ and also check for the latest version at https://mrcieu.github.io/ieugwasr/")
     elif key == 'NO_UID':
         raise Unauthorized("Unable to get uid. Please provide your token.")
     elif key == 'NO_UID_OR_SOURCE':
