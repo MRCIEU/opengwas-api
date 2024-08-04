799f50c1031184e44c869bee43620f0fad25e154
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-08-04 19:52:03 +0100

    feat: Add object copy and PAR creation to OCI object storage

--- app/resources/oci.py
+++ app/resources/oci.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import oci
 
 from resources.globals import Globals
@@ -50,3 +52,47 @@ class OCI:
             bucket_name=Globals.app_config['oci']['object_storage']['bucket_names'][bucket_key],
             object_name=object_name
         )
+
+    def object_storage_par_create(self, bucket_key, object_name, access_type, listing_action: str, expire_in_seconds: int, url_name):
+        """
+        Create Pre-Authenticated Request (PAR) of object storage
+            https://docs.oracle.com/en-us/iaas/api/#/en/objectstorage/20160918/PreauthenticatedRequest/
+            For any object under a directory, set object_name to <dir_name>/ and access_type to AnyObject{Read|Write|ReadWrite}
+        :param bucket_key:
+        :param object_name:
+        :param access_type: ObjectRead | ObjectWrite | ObjectReadWrite | AnyObjectWrite | AnyObjectRead | AnyObjectReadWrite
+        :param listing_action: Deny | ListObjects
+        :param expire_in_seconds:
+        :param url_name: Self-explanatory name for this PAR
+        :return:
+        """
+        return self.object_storage_client.create_preauthenticated_request(
+            namespace_name=Globals.app_config['oci']['object_storage']['namespace_name'],
+            bucket_name=Globals.app_config['oci']['object_storage']['bucket_names'][bucket_key],
+            create_preauthenticated_request_details=oci.object_storage.models.CreatePreauthenticatedRequestDetails(
+                name=url_name,
+                access_type=access_type,
+                time_expires=datetime.now() + timedelta(0, expire_in_seconds),
+                bucket_listing_action=listing_action,
+                object_name=object_name)
+        ).data.full_path
+
+    # Note: this is async
+    def object_storage_copy(self, src_bucket_key, src_object_name, dst_bucket_key, dst_object_name):
+        return self.object_storage_client.copy_object(
+            namespace_name=Globals.app_config['oci']['object_storage']['namespace_name'],
+            bucket_name=Globals.app_config['oci']['object_storage']['bucket_names'][src_bucket_key],
+            copy_object_details=oci.object_storage.models.CopyObjectDetails(
+                source_object_name=src_object_name,
+                destination_region=Globals.app_config['oci']['object_storage']['region'],
+                destination_namespace=Globals.app_config['oci']['object_storage']['namespace_name'],
+                destination_bucket=Globals.app_config['oci']['object_storage']['bucket_names'][dst_bucket_key],
+                destination_object_name=dst_object_name),
+        )
+
+    # Note: this is async
+    def object_storage_copy_by_prefix(self, src_bucket_key, prefix, dst_bucket_key, dst_object_name):
+        object_names = self.object_storage_list(src_bucket_key, prefix)
+        for o in object_names:
+            self.object_storage_copy(src_bucket_key, o, dst_bucket_key, dst_object_name)
+        return object_names
