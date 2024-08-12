0f6855896b4d65cf3c03abdb8103ad085420fd2e
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-08-12 21:07:14 +0100

    feat: Use marshmallow.validate in GroupNodeSchema

--- app/schemas/group_node_schema.py
+++ app/schemas/group_node_schema.py
@@ -1,8 +1,8 @@
-from marshmallow import fields
+from marshmallow import fields, validate
 from marshmallow.exceptions import ValidationError
 from schemas.frpm_schema import FRPMSchema
 
-valid_group_names = {
+valid_group_names = [
     "public",
     "developer",
     "Bristol",
@@ -31,13 +31,13 @@ valid_group_names = {
     "internal",
     "Sclerostin_genetics_consortium",
     "headspace"
-}
+]
 
 
-def check_group_name_is_valid(data):
-    if data not in valid_group_names:
-        raise ValidationError("Group name must be one of: {}".format(valid_group_names))
+# def check_group_name_is_valid(data):
+#     if data not in valid_group_names:
+#         raise ValidationError("Group name must be one of: {}".format(valid_group_names))
 
 
 class GroupNodeSchema(FRPMSchema):
-    name = fields.Str(required=True, allow_none=False, metadata={"description": "Group name"}, validate=check_group_name_is_valid)
+    name = fields.Str(required=True, validate=validate.OneOf(valid_group_names), metadata={"description": "Group name"})
