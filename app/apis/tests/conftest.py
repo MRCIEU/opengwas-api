896e8cdf674779eee0fcf56ca4bbbb39f3eeab34
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-08-19 13:09:22 +0100

    test: Add filelock so that JWT is generated only once

--- app/apis/tests/conftest.py
+++ app/apis/tests/conftest.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.append('..')
 import pytest
+import json
+
+from filelock import FileLock
 
 from resources.globals import Globals
 from apis.tests.token import get_token
@@ -26,11 +29,27 @@ def url(request):
 
 
 @pytest.fixture(scope="session")
-def headers():
+def headers(tmp_path_factory, worker_id):
     # For API tests here, X-TEST-NO-RATE-LIMIT-KEY is provided to bypass flask-limiter while JWT is for authentication
     # For tests from clients e.g. ieugwasr, X-TEST-MODE-KEY is for bypassing flask-limiter and also for authentication, and there is no need to provide JWT. However, when provided, JWT will be used for authentication
     # See app/middleware/auth.py and app/apis/tests/test_user.py
+
+    # https://pytest-xdist.readthedocs.io/en/stable/how-to.html#making-session-scoped-fixtures-execute-only-once
+    if worker_id == "master":
+        # there is only one worker i.e. x-dist inactive
+        token = get_token()
+    else:
+        # x-dist
+        # get the temp directory shared by all workers
+        fn = tmp_path_factory.getbasetemp().parent / "token.json"
+        with FileLock(str(fn) + ".lock"):
+            if fn.is_file():
+                token = json.loads(fn.read_text())['token']
+            else:
+                token = get_token()
+                fn.write_text(json.dumps({'token': token}))
+
     return {
         'X-TEST-NO-RATE-LIMIT-KEY': Globals.app_config['test']['no_rate_limit_key'],
-        'Authorization': 'Bearer ' + get_token()
+        'Authorization': 'Bearer ' + token
     }
