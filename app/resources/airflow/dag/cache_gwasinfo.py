664adb506ace254b5bfaae43ec4906223ad5e87b
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-09-02 19:37:56 +0100

    feat: Add cache_gwasinfo DAG

---
+++ app/resources/airflow/dag/cache_gwasinfo.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+from airflow.models import Variable
+from airflow.operators.http_operator import SimpleHttpOperator
+
+
+@dag(tags=['gwas'], schedule_interval='*/5 * * * *', start_date=datetime(2024, 9, 2, 18, 25))
+def cache_gwasinfo():
+    timeouts = {
+        'cache_gwasinfo': 300
+    }
+
+    cache_gwasinfo = SimpleHttpOperator(
+        task_id='cache_gwasinfo',
+        http_conn_id='api',
+        endpoint='/api/maintenance/gwasinfo/cache',
+        method='GET',
+        headers={
+            'X-SERVICE-KEY': Variable.get("SECRET_API_SERVICE_KEY")
+        },
+        extra_options={
+            'timeout': timeouts['cache_gwasinfo']
+        },
+        log_response=True
+    )
+
+    cache_gwasinfo
+
+cache_gwasinfo()
