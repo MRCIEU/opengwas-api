fa6c9c1e6621902486bb494c20e245a594151e26
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-09-02 19:42:13 +0100

    feat: Update endpoint of 'callback' DAG

--- app/resources/airflow/dag/callback.py
+++ app/resources/airflow/dag/callback.py
@@ -1,15 +1,9 @@
 from datetime import datetime
 
-import requests
-import logging
-
 from airflow.decorators import dag, task
 from airflow.models import Variable
 from airflow.operators.http_operator import SimpleHttpOperator
 
-
-# logging.getLogger('oci').setLevel(logging.DEBUG)
-# logging.basicConfig()
 
 @dag(tags=['gwas'], schedule_interval='*/2 * * * *', start_date=datetime(2024, 8, 22, 8, 40))
 def callback():
@@ -20,7 +14,7 @@ def callback():
     refresh_added_by_status = SimpleHttpOperator(
         task_id='refresh_added_by_status',
         http_conn_id='api',
-        endpoint='/api/maintenance/refresh_added_by_status',
+        endpoint='/api/maintenance/pipeline/added_by_state/refresh',
         method='GET',
         headers={
             'X-SERVICE-KEY': Variable.get("SECRET_API_SERVICE_KEY")
