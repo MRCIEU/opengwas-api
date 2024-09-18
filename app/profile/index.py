5370836bedb3d3ddc0755cb17ad8f533591ae1dd
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2024-09-18 11:15:49 +0100

    feat: Remove org info display on the profile page

--- app/profile/index.py
+++ app/profile/index.py
@@ -12,17 +12,8 @@ profile_index_bp = Blueprint('index', __name__)
 @profile_index_bp.route('')
 @login_required
 def index():
-    org, membership = get_org_and_membership_from_user(current_user['uid']) if current_user['tier'] == 'ORG' else (None, None)
-    org_tooltip = ""
-    if org:
-        if membership:
-            org_tooltip = "The following information was provided by Microsoft when you use Single Sign-On (SSO)."
-        else:
-            org_tooltip = "The following information was inferred from the domain name of your email address."
-
     g.user = current_user
-    return render_template('profile/index.html',
-                           user=current_user, globals_sources=Globals.USER_SOURCES, org=org, org_tooltip=org_tooltip, membership=membership,
+    return render_template('profile/index.html', user=current_user, globals_sources=Globals.USER_SOURCES,
                            allowance_by_user_source=get_allowance_by_user_source(), root_url=Globals.app_config['root_url'])
 
 
