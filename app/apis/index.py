d6a5c9759ae77a5f22be6f168e9bed74f767685a
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-01-09 11:00:37 +0000

    feat: Remove status badges and add links to /api/status and Pulsetic to API tutorial

--- app/apis/index.py
+++ app/apis/index.py
@@ -5,11 +5,7 @@ from .status import check_all, count_elastic_records, count_neo4j_datasets
 
 
 def index():
-    status = check_all()
-    elastic_counts = count_elastic_records()
-    neo4j_counts = count_neo4j_datasets()
     return flask.render_template('api/index.html',
-                                 status=status, elastic_counts=elastic_counts, neo4j_counts=neo4j_counts,
                                  root_url=Globals.app_config['root_url'],
                                  user_tiers=Globals.USER_TIERS, user_allowance=Globals.ALLOWANCE_BY_USER_TIER,
                                  jwt_validity=int(Globals.JWT_VALIDITY / 86400))
