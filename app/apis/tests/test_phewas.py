5882bf4ad2b73bbfd28eba6e60e1c3e24536d6e8
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-05-19 21:11:09 +0100

    test: Use chunked query for /api/phewas

--- app/apis/tests/test_phewas.py
+++ app/apis/tests/test_phewas.py
@@ -23,7 +23,7 @@ def test_phewas_get2(url, headers):
     assert r.status_code == 200 and len(r.json()) > 200
 
 
-def test_phewas_post0(url, headers):
+def test_phewas_post_unauth(url, headers):
     headers = headers.copy()
     del headers['Authorization']
     payload = {'variant': 'rs234', 'pval': 0.001}
@@ -31,34 +31,53 @@ def test_phewas_post0(url, headers):
     assert r.status_code == 401
 
 
-def test_phewas_post1(url, headers):
-    payload = {'variant': 'rs234', 'pval': 0.001}
+def test_phewas_post_large_pval(url, headers):
+    payload = {'variant': 'rs234', 'pval': 0.01}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
-    assert r.status_code == 200 and len(r.json()) > 20
+    assert r.status_code == 400
 
 
+def test_phewas_post_too_many_records(url, headers):
+    payload = {'variant': 'rs234', 'pval': 0.009}
+    r = requests.post(url + "/phewas", data=payload, headers=headers)
+    assert r.status_code == 400
+
+
+def test_phewas_post_rsid(url, headers):
+    payload = {'variant': 'rs234', 'pval': 0.0005}
+    r = requests.post(url + "/phewas", data=payload, headers=headers)
+    r_json = r.json()
+    assert r.status_code == 200 and len(r_json) > 15 and all(float(a['p']) < 0.001 for a in r_json)
+
+
+@pytest.mark.skip
 def test_phewas_post2(url, headers):
     payload = {'variant': 'rs234', 'pval': 0.01}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
     assert r.status_code == 200 and len(r.json()) > 200
 
 
-def test_phewas_post3(url, headers):
-    payload = {'variant': '7:105561135', 'pval': 0.01}
+def test_phewas_post_chrpos(url, headers):
+    payload = {'variant': '7:105561135', 'pval': 0.0005}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
-    assert r.status_code == 200 and len(r.json()) > 200
+    r_json = r.json()
+    assert r.status_code == 200 and len(r_json) > 15 and all(float(a['p']) < 0.001 for a in r_json)
 
 
-def test_phewas_post4(url, headers):
-    payload = {'variant': '7:105561135-105563135', 'pval': 0.01}
+def test_phewas_post_cprange(url, headers):
+    payload = {'variant': '7:105561135-105563135', 'pval': 0.00005}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
-    assert r.status_code == 200 and len(r.json()) > 400
+    r_json = r.json()
+    assert r.status_code == 200 and len(r_json) > 15 and all(float(a['p']) < 0.0001 for a in r_json)
 
 
-def test_phewas_post5(url, headers):
+def test_phewas_post_batch_filter(url, headers):
     payload = {'variant': 'rs1205', 'pval': 0.0001, 'index_list': ['ieu-a']}
     r = requests.post(url + "/phewas", data=payload, headers=headers)
-    assert r.status_code == 200 and len(r.json()) == 1
+    r_json = r.json()
+    assert (r.status_code == 200 and len(r_json) < 5 and all(float(a['p']) < 0.0001 for a in r_json)
+            and all(a['id'].startswith('ieu-a') for a in r_json))
+
 
 def test_phewas_post6(url, headers):
     payload = {'variant': 'rs1693457', 'pval': 0.00000005, 'index_list': ['ubm-a']}
