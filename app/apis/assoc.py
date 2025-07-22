d42736a9e691763e99cb522d70099c4a87275ddb
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-07-22 15:08:56 +0100

    feat: Add after request handlers

--- app/apis/assoc.py
+++ app/apis/assoc.py
@@ -1,9 +1,9 @@
 from flask import g
 from flask_restx import Resource, reqparse, abort, Namespace
-import math
 import time
 import traceback
 
+from middleware.after_request import compare_results, fix_legacy_value_errors
 from middleware.auth import jwt_required
 from middleware.limiter import limiter, get_allowance_by_user_tier, get_key_func_uid
 from middleware.logger import logger as logger_middleware
@@ -17,49 +17,8 @@ def _get_cost(ids=None, variants=None, proxies=0):  # Note: both inputs should b
     return max(len(ids), len(variants)) * (1 if proxies == 1 else 5)
 
 
-def _compare_results(r0, r1):
-    r0fset = {frozenset(a.items()) for a in r0}
-    r1fset = {frozenset(a.items()) for a in r1}
-
-    r0uniq = r0fset - r1fset
-    r1uniq = r1fset - r0fset
-
-    anomalies = []
-
-    r0uniq = [{a[0]: a[1] for a in fset} for fset in r0uniq]
-    r0uniq_dict = {f"{a['id']}_{a['chr']}_{a['position']}_{a['rsid']}_{a['ea']}_{a['nea']}": a for a in r0uniq}
-    if len(r0uniq_dict) != len(r0uniq):
-        anomalies.append(['DUPLICATE_ID', 'r0'])
-
-    r1uniq = [{a[0]: a[1] for a in fset} for fset in r1uniq]
-    r1uniq_dict = {f"{a['id']}_{a['chr']}_{a['position']}_{a['rsid']}_{a['ea']}_{a['nea']}": a for a in r1uniq}
-    if len(r1uniq_dict) != len(r1uniq):
-        anomalies.append(['DUPLICATE_ID', 'r1'])
-
-    print(len(r0uniq_dict), len(r1uniq_dict))
-
-    def _compare(dict1, dict2):
-        for id in dict1.keys():
-            if id not in dict2:
-                anomalies.append(['MISSING_ID', id])
-            elif dict1[id] != dict2[id]:
-                diff_keys = {k for k in dict1[id] if dict1[id][k] != dict2[id][k]}
-                if not (
-                    (diff_keys == {'n'} and int(float('0' + str(dict1[id]['n']))) == int(float('0' + str(dict2[id]['n'])))) or
-                    (diff_keys == {'beta', 'eaf'} and
-                        math.isclose(float(dict1[id]['beta']) * -1, float(dict2[id]['beta']), rel_tol=1e-6) and
-                        math.isclose(float(dict1[id]['eaf']) + float(dict2[id]['eaf']), 1.000000, rel_tol=1e-6)
-                    )
-                ):
-                    anomalies.append(['DIFF_IN_VALUE', dict1[id], dict2[id], list(diff_keys)])
-
-    _compare(r0uniq_dict, r1uniq_dict)
-    _compare(r1uniq_dict, r0uniq_dict)
-
-    return anomalies
-
-
 @api.route('/<id>/<variant>')
+@api.hide
 @api.doc(
     description="Get specific variant associations for specifc GWAS datasets",
     params={
@@ -153,7 +112,7 @@ class AssocPost(Resource):
                               len(result_from_chunks), list(set([r['id'] for r in result_from_chunks])), len(set([r['rsid'] for r in result_from_chunks])))
 
         # try:
-        #     anomalies = _compare_results(result, result_from_chunks)
+        #     anomalies = compare_results(result, result_from_chunks)
         #     assert anomalies == []
         # except AssertionError as e:
         #     log_timestamp = logger_middleware.log_error(g.user['uuid'], 'assoc_post', args, anomalies)
@@ -163,48 +122,4 @@ class AssocPost(Resource):
         #         'error_log_timestamp': log_timestamp
         #     }, 503
 
-        return result_from_chunks
-
-
-@api.route('/chunked')
-@api.doc(
-    description="Get specific variant associations for specifc GWAS datasets (Implementation of chunking backed by object storage - beta version)"
-)
-class AssocChunkedPost(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('variant', required=False, type=str, action='append', default=[],
-                         help="List of variants as rsid or chr:pos or chr:pos1-pos2 where positions are in hg19/b37 e.g. ['rs1205', '7:105561135', '7:105561135-105563135']")
-    parser.add_argument('id', required=False, type=str, action='append', default=[], help="list of GWAS study IDs")
-    parser.add_argument('proxies', type=int, required=False, default=0,
-                         help="Whether to look for proxies (1) or not (0). Note that proxies won't be looked for range queries")
-    parser.add_argument('r2', type=float, required=False, default=0.8, help="Minimum LD r2 for a proxy")
-    parser.add_argument('align_alleles', type=int, required=False, default=1, help="Whether to align alleles")
-    parser.add_argument('palindromes', type=int, required=False, default=1,
-                         help="Whether to allow palindromic proxies")
-    parser.add_argument('maf_threshold', type=float, required=False, default=0.3,
-                         help="Maximum MAF allowed for a palindromic variant")
-
-    @api.expect(parser)
-    @api.doc(id='assoc_chunked_post')
-    @jwt_required
-    def post(self):
-        args = self.parser.parse_args()
-
-        with limiter.shared_limit(limit_value=get_allowance_by_user_tier, scope='allowance_by_user_tier', key_func=get_key_func_uid, cost=_get_cost(args['id'], args['variant'], args['proxies'])):
-            pass
-
-        if len(args['id']) == 0 or len(args['variant']) == 0:
-            abort(400)
-
-        start_time = time.time()
-
-        try:
-            result, n_chunks_accessed = get_assoc_chunked(g.user['uid'], args['variant'], args['id'], args['proxies'], args['r2'], args['align_alleles'], args['palindromes'], args['maf_threshold'])
-        except Exception as e:
-            logger.error("Could not obtain SNP association: {}".format(e))
-            abort(503)
-
-        logger_middleware.log(g.user['uuid'], 'assoc_chunked_post', start_time,
-                              {'id': len(args['id']), 'variant': len(args['variant']), 'proxies': args['proxies'], 'chunks': n_chunks_accessed},
-                              len(result), list(set([r['id'] for r in result])), len(set([r['rsid'] for r in result])))
-        return result
+        return fix_legacy_value_errors(result_from_chunks)
