1115ef09bf50dbff3bae43a3fb158ddb3f4c5cc4
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-07-27 15:15:59 +0100

    feat: Remove deprecated endpoints from phewas

--- app/apis/phewas.py
+++ app/apis/phewas.py
@@ -29,74 +29,74 @@ def _get_response_cost(result):
     return len(result)
 
 
-@api.route('/<variant>/<pval>')
-@api.hide
-@api.doc(
-    description="Perform PheWAS of specified variants across all available GWAS datasets",
-    params={
-        'variant': "Comma-separated list of rs IDs, chr:pos or chr:pos range  (hg19/b37). e.g rs1205,7:105561135,7:105561135-105563135",
-        'pval': "P-value threshold. Default = 1e-3"
-    }
-)
-class PhewasGet(Resource):
-    parser = api.parser()
-
-    @api.expect(parser)
-    @api.doc(id='phewas_get')
-    @jwt_required
-    def get(self, variant='', pval=1e-3):
-        variants = variant.split(',')
-
-        with limiter.shared_limit(limit_value=get_allowance_by_user_tier, scope='allowance_by_user_tier', key_func=get_key_func_uid, cost=_get_cost(variants)):
-            pass
-
-        if variants == ['']:
-            abort(400)
-
-        start_time = time.time()
-
-        try:
-            result = run_phewas(user_email=g.user['uid'], variants=variants, pval=float(pval), index_list=[])
-        except Exception as e:
-            logger.error("Could not query summary stats: {}".format(e))
-            abort(503)
-
-        logger_middleware.log(g.user['uuid'], 'phewas_get', start_time, {'variant': len(variants)},
-                              len(result), list(set([r['id'] for r in result])), len(set([r['rsid'] for r in result])))
-        return result
-
-
-@api.route('/slow')
-@api.deprecated
-@api.doc(
-    description="Perform PheWAS of specified variants across all available GWAS datasets."
-)
-class PhewasPost(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('variant', required=False, type=str, action='append', default=[], help="List of rs IDs, chr:pos or chr:pos range  (hg19/b37). e.g rs1205,7:105561135,7:105561135-105563135")
-    parser.add_argument('pval', type=float, required=False, default=1e-05, help='P-value threshold')
-    parser.add_argument('index_list', required=False, type=str, action='append', default=[], help="List of study indexes. If empty then searches across all indexes.")
-
-    @api.expect(parser)
-    @api.doc(id='phewas_post')
-    @jwt_required
-    def post(self):
-        args = self.parser.parse_args()
-
-        with limiter.shared_limit(limit_value=get_allowance_by_user_tier, scope='allowance_by_user_tier', key_func=get_key_func_uid, cost=_get_cost(args['variant'])):
-            pass
-
-        start_time = time.time()
-
-        try:
-            result = run_phewas(user_email=g.user['uid'], variants=args['variant'], pval=args['pval'], index_list=args['index_list'])
-        except Exception as e:
-            logger.error("Could not query summary stats: {}".format(e))
-            abort(503)
-
-        logger_middleware.log(g.user['uuid'], 'phewas_post', start_time, {'variant': len(args['variant'])},
-                              len(result), list(set([r['id'] for r in result])), len(set([r['rsid'] for r in result])))
-        return result
+# @api.route('/<variant>/<pval>')
+# @api.hide
+# @api.doc(
+#     description="Perform PheWAS of specified variants across all available GWAS datasets",
+#     params={
+#         'variant': "Comma-separated list of rs IDs, chr:pos or chr:pos range  (hg19/b37). e.g rs1205,7:105561135,7:105561135-105563135",
+#         'pval': "P-value threshold. Default = 1e-3"
+#     }
+# )
+# class PhewasGet(Resource):
+#     parser = api.parser()
+#
+#     @api.expect(parser)
+#     @api.doc(id='phewas_get')
+#     @jwt_required
+#     def get(self, variant='', pval=1e-3):
+#         variants = variant.split(',')
+#
+#         with limiter.shared_limit(limit_value=get_allowance_by_user_tier, scope='allowance_by_user_tier', key_func=get_key_func_uid, cost=_get_cost(variants)):
+#             pass
+#
+#         if variants == ['']:
+#             abort(400)
+#
+#         start_time = time.time()
+#
+#         try:
+#             result = run_phewas(user_email=g.user['uid'], variants=variants, pval=float(pval), index_list=[])
+#         except Exception as e:
+#             logger.error("Could not query summary stats: {}".format(e))
+#             abort(503)
+#
+#         logger_middleware.log(g.user['uuid'], 'phewas_get', start_time, {'variant': len(variants)},
+#                               len(result), list(set([r['id'] for r in result])), len(set([r['rsid'] for r in result])))
+#         return result
+#
+#
+# @api.route('/slow')
+# @api.deprecated
+# @api.doc(
+#     description="Perform PheWAS of specified variants across all available GWAS datasets."
+# )
+# class PhewasPost(Resource):
+#     parser = reqparse.RequestParser()
+#     parser.add_argument('variant', required=False, type=str, action='append', default=[], help="List of rs IDs, chr:pos or chr:pos range  (hg19/b37). e.g rs1205,7:105561135,7:105561135-105563135")
+#     parser.add_argument('pval', type=float, required=False, default=1e-05, help='P-value threshold')
+#     parser.add_argument('index_list', required=False, type=str, action='append', default=[], help="List of study indexes. If empty then searches across all indexes.")
+#
+#     @api.expect(parser)
+#     @api.doc(id='phewas_post')
+#     @jwt_required
+#     def post(self):
+#         args = self.parser.parse_args()
+#
+#         with limiter.shared_limit(limit_value=get_allowance_by_user_tier, scope='allowance_by_user_tier', key_func=get_key_func_uid, cost=_get_cost(args['variant'])):
+#             pass
+#
+#         start_time = time.time()
+#
+#         try:
+#             result = run_phewas(user_email=g.user['uid'], variants=args['variant'], pval=args['pval'], index_list=args['index_list'])
+#         except Exception as e:
+#             logger.error("Could not query summary stats: {}".format(e))
+#             abort(503)
+#
+#         logger_middleware.log(g.user['uuid'], 'phewas_post', start_time, {'variant': len(args['variant'])},
+#                               len(result), list(set([r['id'] for r in result])), len(set([r['rsid'] for r in result])))
+#         return result
 
 
 @api.route('')
@@ -139,39 +139,39 @@ class PhewasFastPost(Resource):
         return result
 
 
-def run_phewas(user_email, variants, pval, index_list=None):
-    variants = organise_variants(variants)
-
-    rsid = variants['rsid']
-    chrpos = variants['chrpos']
-    cprange = variants['cprange']
-
-    allres = []
-    if len(rsid) > 0:
-        try:
-            res = elastic_query_phewas_rsid(rsid=rsid, user_email=user_email, pval=pval, index_list=index_list)
-            allres += res
-        except Exception as e:
-            logging.error("Could not obtain summary stats: {}".format(e))
-            flask.abort(503, e)
-
-    if len(chrpos) > 0:
-        try:
-            res = elastic_query_phewas_chrpos(chrpos=chrpos, user_email=user_email, pval=pval, index_list=index_list)
-            allres += res
-        except Exception as e:
-            logging.error("Could not obtain summary stats: {}".format(e))
-            flask.abort(503, e)
-
-    if len(cprange) > 0:
-        try:
-            res = elastic_query_phewas_cprange(cprange=cprange, user_email=user_email, pval=pval, index_list=index_list)
-            allres += res
-        except Exception as e:
-            logging.error("Could not obtain summary stats: {}".format(e))
-            flask.abort(503, e)
-
-    return allres
+# def run_phewas(user_email, variants, pval, index_list=None):
+#     variants = organise_variants(variants)
+#
+#     rsid = variants['rsid']
+#     chrpos = variants['chrpos']
+#     cprange = variants['cprange']
+#
+#     allres = []
+#     if len(rsid) > 0:
+#         try:
+#             res = elastic_query_phewas_rsid(rsid=rsid, user_email=user_email, pval=pval, index_list=index_list)
+#             allres += res
+#         except Exception as e:
+#             logging.error("Could not obtain summary stats: {}".format(e))
+#             flask.abort(503, e)
+#
+#     if len(chrpos) > 0:
+#         try:
+#             res = elastic_query_phewas_chrpos(chrpos=chrpos, user_email=user_email, pval=pval, index_list=index_list)
+#             allres += res
+#         except Exception as e:
+#             logging.error("Could not obtain summary stats: {}".format(e))
+#             flask.abort(503, e)
+#
+#     if len(cprange) > 0:
+#         try:
+#             res = elastic_query_phewas_cprange(cprange=cprange, user_email=user_email, pval=pval, index_list=index_list)
+#             allres += res
+#         except Exception as e:
+#             logging.error("Could not obtain summary stats: {}".format(e))
+#             flask.abort(503, e)
+#
+#     return allres
 
 
 def run_phewas_fast(user_email, variants, pval, batch_list=None):
