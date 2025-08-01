586862d9e33cb40271d19f9da11e547c9eab9c81
JasonQiu <22163445+JasonQiu@users.noreply.github.com>
2025-08-01 10:25:30 +0100

    chore: Reformat gunicorn.sh

--- app/gunicorn.sh
+++ app/gunicorn.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gunicorn --bind=0.0.0.0:80 --timeout=$GUNICORN_TIMEOUT --worker-class=gevent --workers=$(($(nproc) + 1)) main:app
+gunicorn --bind=0.0.0.0:80 \
+ --timeout=$GUNICORN_TIMEOUT \
+ --worker-class=gevent --workers=$(($(nproc) + 1)) \
+ main:app
